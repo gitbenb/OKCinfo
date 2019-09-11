3331bc1849ee234231af17c90643a86dcac692a4
Ben Borges <email@benborges.xyz>
2019-09-11 10:18:12 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 838,
-	"created_at": "Mon Sep 02 08:18:12 +0000 2019",
+	"tweets": 839,
+	"created_at": "Wed Sep 11 08:18:11 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_09.js
+++ data/js/tweets/2019_09.js
@@ -1,6 +1,235 @@
 Grailbird.data.tweets_2019_09 = 
 [
 	{
+		"created_at": "Tue Sep 10 12:05:41 +0000 2019",
+		"id": 1171394307556860000,
+		"id_str": "1171394307556859904",
+		"full_text": "Question pour @MatthieuRicard : les abus sexuels dans le bouddhisme tibétain : ( RIGPA,OKC, SHAMBHALA, TRIRATNA,) c'est toujours une \"polémique\" pour lui (d'après son dernier émail on dirait bien) ou bien quoi ? On le publie en public cet échange ? Ou on vient poser la question ? https://t.co/MTbZ5PqbbT",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			280
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "MatthieuRicard",
+					"name": "Matthieu Ricard",
+					"id": 181228051,
+					"id_str": "181228051",
+					"indices": [
+						14,
+						29
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/MTbZ5PqbbT",
+					"expanded_url": "https://twitter.com/RencontresPersp/status/1169304399145046016",
+					"display_url": "twitter.com/RencontresPers…",
+					"indices": [
+						281,
+						304
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3309353195,
+			"id_str": "3309353195",
+			"name": "#OKCinfo",
+			"screen_name": "OKCinformation",
+			"location": "Belgique",
+			"description": "#OKCinfo is a 2015 initiative from ex-OKC born kids now civil parties to drag Ogyen Kunzang Choling/Robert Spatz @ogyenkun to Justice | okcinfor@gmail.com",
+			"url": "https://t.co/iXnsHdew7Q",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/iXnsHdew7Q",
+							"expanded_url": "http://www.okcinfo.news/",
+							"display_url": "okcinfo.news",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 149,
+			"friends_count": 357,
+			"listed_count": 8,
+			"created_at": "Fri Jun 05 15:00:16 +0000 2015",
+			"favourites_count": 262,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 832,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/656530168312262656/Ujovmh2w_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/656530168312262656/Ujovmh2w_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3309353195/1526300811",
+			"profile_link_color": "DD2E44",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1169304399145046000,
+		"quoted_status_id_str": "1169304399145046016",
+		"quoted_status_permalink": {
+			"url": "https://t.co/MTbZ5PqbbT",
+			"expanded": "https://twitter.com/RencontresPersp/status/1169304399145046016",
+			"display": "twitter.com/RencontresPers…"
+		},
+		"quoted_status": {
+			"created_at": "Wed Sep 04 17:41:08 +0000 2019",
+			"id": 1169304399145046000,
+			"id_str": "1169304399145046016",
+			"full_text": "Plus que 5 jours, avant la conférence de Yongey Mingyur Rinpoche &amp; Matthieu Ricard ! \n\nUne conférence sur le thème du dernier livre de Yongey Mingyur Rinpoche « Pour l'amour du monde » \nLa conférence étant complète, nous avons mis en place un Streaming : \nhttps://t.co/mJpFqyfLNx",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				283
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/mJpFqyfLNx",
+						"expanded_url": "https://www.crowdcast.io/lesrencontresperspectives",
+						"display_url": "crowdcast.io/lesrencontresp…",
+						"indices": [
+							260,
+							283
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 2351961654,
+				"id_str": "2351961654",
+				"name": "RencontresPerspectiv",
+				"screen_name": "RencontresPersp",
+				"location": "",
+				"description": "Organisateur de conférences pour des auteurs inspirants : Christophe André, Matthieu Ricard, Frédéric Lenoir...",
+				"url": "https://t.co/6KFsS5hOXa",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/6KFsS5hOXa",
+								"expanded_url": "http://www.rencontres-perspectives.fr",
+								"display_url": "rencontres-perspectives.fr",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 468,
+				"friends_count": 183,
+				"listed_count": 4,
+				"created_at": "Wed Feb 19 16:49:39 +0000 2014",
+				"favourites_count": 2487,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 812,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/905454647728644098/vrBX3GDp_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/905454647728644098/vrBX3GDp_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/2351961654/1504712210",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 13,
+			"favorite_count": 24,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "fr"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "fr",
+		"text": "Question pour @MatthieuRicard : les abus sexuels dans le bouddhisme tibétain : ( RIGPA,OKC, SHAMBHALA, TRIRATNA,) c'est toujours une \"polémique\" pour lui (d'après son dernier émail on dirait bien) ou bien quoi ? On le publie en public cet échange ? Ou on vient poser la question ? https://t.co/MTbZ5PqbbT"
+	},
+	{
 		"created_at": "Sun Sep 01 10:12:46 +0000 2019",
 		"id": 1168104403724832800,
 		"id_str": "1168104403724832770",
