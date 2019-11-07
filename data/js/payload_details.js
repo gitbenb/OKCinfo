80a741e30eccfeb5e4e37e1310799b5df361f0be
Ben Borges <email@benborges.xyz>
2019-11-07 09:18:12 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 864,
-	"created_at": "Tue Oct 29 08:18:11 +0000 2019",
+	"tweets": 865,
+	"created_at": "Thu Nov 07 08:18:11 +0000 2019",
 	"lang": "en"
 }
---
+++ data/js/tweets/2019_11.js
@@ -0,0 +1,128 @@
+Grailbird.data.tweets_2019_11 = 
+[
+	{
+		"created_at": "Wed Nov 06 17:28:33 +0000 2019",
+		"id": 1192124462369362000,
+		"id_str": "1192131671744757760",
+		"full_text": "En solidarité avec toutes les femmes qui parlent #MeToo: Adèle Haenel explique en direct pourquoi elle sort du silence https://t.co/rhy9DpuKvU (https://t.co/V5GF5285Q8)",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			168
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "MeToo",
+					"indices": [
+						49,
+						55
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/rhy9DpuKvU",
+					"expanded_url": "https://vimeo.com/370827280",
+					"display_url": "vimeo.com/370827280",
+					"indices": [
+						119,
+						142
+					]
+				},
+				{
+					"url": "https://t.co/V5GF5285Q8",
+					"expanded_url": "https://okcinfo.news/archives/s/1PpDOA",
+					"display_url": "okcinfo.news/archives/s/1Pp…",
+					"indices": [
+						144,
+						167
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://okcinfo.news/archives/\" rel=\"nofollow\">OKCinfotweets</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1097520031955587100,
+			"id_str": "1097520031955587073",
+			"name": "OKCinfo",
+			"screen_name": "OKCinfor",
+			"location": "Brussel, België",
+			"description": "An initiative to restore the truths against the toxic OKC Cult where physical, sexual & mental abuses enfolded for decades. Trial & Appeal ongoing check website",
+			"url": "https://t.co/feER16PibH",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/feER16PibH",
+							"expanded_url": "https://www.okcinfo.news",
+							"display_url": "okcinfo.news",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 48,
+			"friends_count": 236,
+			"listed_count": 1,
+			"created_at": "Mon Feb 18 15:35:41 +0000 2019",
+			"favourites_count": 59,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 265,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1140749925715775488/-ruh0TMj_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1140749925715775488/-ruh0TMj_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1097520031955587073/1562445469",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": true,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "fr",
+		"text": "En solidarité avec toutes les femmes qui parlent #MeToo: Adèle Haenel explique en direct pourquoi elle sort du silence https://t.co/rhy9DpuKvU (https://t.co/V5GF5285Q8)"
+	}
+]
