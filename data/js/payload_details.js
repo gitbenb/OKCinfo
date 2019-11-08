7002305c04ab751fecfd147bac8c0e6255146749
Ben Borges <email@benborges.xyz>
2019-11-08 09:18:12 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 865,
-	"created_at": "Thu Nov 07 08:18:11 +0000 2019",
+	"tweets": 866,
+	"created_at": "Fri Nov 08 08:18:11 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_11.js
+++ data/js/tweets/2019_11.js
@@ -1,6 +1,390 @@
 Grailbird.data.tweets_2019_11 = 
 [
 	{
+		"created_at": "Fri Nov 08 06:28:23 +0000 2019",
+		"id": 1192683867796394000,
+		"id_str": "1192690307785252865",
+		"full_text": "Enfin une bonne nouvelle judiciaire en Belgique ! https://t.co/cThyKSurPJ",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			49
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/cThyKSurPJ",
+					"expanded_url": "https://twitter.com/PolBegov/status/1192492622788669440",
+					"display_url": "twitter.com/PolBegov/statu…",
+					"indices": [
+						50,
+						73
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1097520031955587100,
+			"id_str": "1097520031955587073",
+			"name": "OKCinfo",
+			"screen_name": "OKCinfor",
+			"location": "Brussel, België",
+			"description": "An initiative to restore the truths against the toxic OKC Cult where physical, sexual & mental abuses enfolded for decades. Trial & Appeal ongoing check website",
+			"url": "https://t.co/feER16PibH",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/feER16PibH",
+							"expanded_url": "https://www.okcinfo.news",
+							"display_url": "okcinfo.news",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 47,
+			"friends_count": 236,
+			"listed_count": 1,
+			"created_at": "Mon Feb 18 15:35:41 +0000 2019",
+			"favourites_count": 62,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 266,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1140749925715775488/-ruh0TMj_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1140749925715775488/-ruh0TMj_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1097520031955587073/1562445469",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": true,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1192492622788669400,
+		"quoted_status_id_str": "1192492622788669440",
+		"quoted_status_permalink": {
+			"url": "https://t.co/cThyKSurPJ",
+			"expanded": "https://twitter.com/PolBegov/status/1192492622788669440",
+			"display": "twitter.com/PolBegov/statu…"
+		},
+		"quoted_status": {
+			"created_at": "Thu Nov 07 17:22:51 +0000 2019",
+			"id": 1192492622788669400,
+			"id_str": "1192492622788669440",
+			"full_text": "La Chambre approuve l’imprescriptibilité des délits sexuels graves sur mineurs Le texte a recueilli l’unanimité moins l’abstention des écologistes et du député indépendant Jean-Marie Dedecker. https://t.co/8h7qmtivKw https://t.co/05KWqI9ljE",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				216
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/8h7qmtivKw",
+						"expanded_url": "https://www.lesoir.be/259003/article/2019-11-07/la-chambre-approuve-limprescriptibilite-des-delits-sexuels-graves-sur-mineurs",
+						"display_url": "lesoir.be/259003/article…",
+						"indices": [
+							193,
+							216
+						]
+					}
+				],
+				"media": [
+					{
+						"id": 1192492620779544600,
+						"id_str": "1192492620779544578",
+						"indices": [
+							217,
+							240
+						],
+						"media_url": "http://pbs.twimg.com/media/EIyVuHLW4AID646.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EIyVuHLW4AID646.jpg",
+						"url": "https://t.co/05KWqI9ljE",
+						"display_url": "pic.twitter.com/05KWqI9ljE",
+						"expanded_url": "https://twitter.com/PolBegov/status/1192492622788669440/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 1191,
+								"h": 669,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1191,
+								"h": 669,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 382,
+								"resize": "fit"
+							}
+						},
+						"features": {
+							"orig": {
+								"faces": [
+									{
+										"x": 574,
+										"y": 305,
+										"h": 88,
+										"w": 88
+									}
+								]
+							},
+							"large": {
+								"faces": [
+									{
+										"x": 574,
+										"y": 305,
+										"h": 88,
+										"w": 88
+									}
+								]
+							},
+							"medium": {
+								"faces": [
+									{
+										"x": 574,
+										"y": 305,
+										"h": 88,
+										"w": 88
+									}
+								]
+							},
+							"small": {
+								"faces": [
+									{
+										"x": 327,
+										"y": 174,
+										"h": 50,
+										"w": 50
+									}
+								]
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1192492620779544600,
+						"id_str": "1192492620779544578",
+						"indices": [
+							217,
+							240
+						],
+						"media_url": "http://pbs.twimg.com/media/EIyVuHLW4AID646.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EIyVuHLW4AID646.jpg",
+						"url": "https://t.co/05KWqI9ljE",
+						"display_url": "pic.twitter.com/05KWqI9ljE",
+						"expanded_url": "https://twitter.com/PolBegov/status/1192492622788669440/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 1191,
+								"h": 669,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1191,
+								"h": 669,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 382,
+								"resize": "fit"
+							}
+						},
+						"features": {
+							"orig": {
+								"faces": [
+									{
+										"x": 574,
+										"y": 305,
+										"h": 88,
+										"w": 88
+									}
+								]
+							},
+							"large": {
+								"faces": [
+									{
+										"x": 574,
+										"y": 305,
+										"h": 88,
+										"w": 88
+									}
+								]
+							},
+							"medium": {
+								"faces": [
+									{
+										"x": 574,
+										"y": 305,
+										"h": 88,
+										"w": 88
+									}
+								]
+							},
+							"small": {
+								"faces": [
+									{
+										"x": 327,
+										"y": 174,
+										"h": 50,
+										"w": 50
+									}
+								]
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twibble.io\" rel=\"nofollow\">Twibble.io</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 2359778089,
+				"id_str": "2359778089",
+				"name": "Veille infopol",
+				"screen_name": "PolBegov",
+				"location": "Bloqué par Théo Francken",
+				"description": "NewsBot Veille Info 🇧🇪\nAlimenté par les sections politique, économie et région des différents médias en Belgique. FR/NL/EN",
+				"url": "https://t.co/0cz5mWgAHw",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/0cz5mWgAHw",
+								"expanded_url": "https://www.euwatch.live/polbegov-begov/",
+								"display_url": "euwatch.live/polbegov-begov/",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 4175,
+				"friends_count": 2116,
+				"listed_count": 244,
+				"created_at": "Mon Feb 24 16:54:12 +0000 2014",
+				"favourites_count": 3865,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 195276,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "1A1B1F",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/830406487851802625/x_YH1yml_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/830406487851802625/x_YH1yml_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/2359778089/1448275480",
+				"profile_link_color": "DD2E44",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "C0DFEC",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"can_media_tag": true,
+				"followed_by": true,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 6,
+			"favorite_count": 9,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "fr"
+		},
+		"retweet_count": 3,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "fr",
+		"text": "Enfin une bonne nouvelle judiciaire en Belgique ! https://t.co/cThyKSurPJ"
+	},
+	{
 		"created_at": "Wed Nov 06 17:28:33 +0000 2019",
 		"id": 1192124462369362000,
 		"id_str": "1192131671744757760",
