76e4f04afab27b2b7be3e85043d41b5d60c04c58
Ben Borges <email@benborges.xyz>
2019-12-17 09:18:12 +0100

    Added 1 tweets

---
+++ data/js/tweets/2019_12.js
@@ -0,0 +1,292 @@
+Grailbird.data.tweets_2019_12 = 
+[
+	{
+		"created_at": "Mon Dec 16 09:53:28 +0000 2019",
+		"id": 1206505359621402600,
+		"id_str": "1206512660021469185",
+		"full_text": "Tibetan Nyigma-Pa support sexual predators : Takloung Matrul Rinpotché à Bxl https://t.co/BS6TSP3DtT via @ogyenkun #metoo https://t.co/tb2xUQGNmd",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			121
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "metoo",
+					"indices": [
+						115,
+						121
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "OgyenKun",
+					"name": "OKC",
+					"id": 3665270417,
+					"id_str": "3665270417",
+					"indices": [
+						105,
+						114
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/BS6TSP3DtT",
+					"expanded_url": "http://okc-net.org/index.php/event/takloung-matrul-rinpotche-2020/",
+					"display_url": "okc-net.org/index.php/even…",
+					"indices": [
+						77,
+						100
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1206505358157586400,
+					"id_str": "1206505358157586432",
+					"indices": [
+						122,
+						145
+					],
+					"media_url": "http://pbs.twimg.com/media/EL5eOxyWoAA29in.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EL5eOxyWoAA29in.jpg",
+					"url": "https://t.co/tb2xUQGNmd",
+					"display_url": "pic.twitter.com/tb2xUQGNmd",
+					"expanded_url": "https://twitter.com/OKCinfor/status/1206505359621402624/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 800,
+							"h": 533,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 800,
+							"h": 533,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"medium": {
+							"faces": [
+								{
+									"x": 320,
+									"y": 89,
+									"h": 218,
+									"w": 218
+								}
+							]
+						},
+						"orig": {
+							"faces": [
+								{
+									"x": 320,
+									"y": 89,
+									"h": 218,
+									"w": 218
+								}
+							]
+						},
+						"large": {
+							"faces": [
+								{
+									"x": 320,
+									"y": 89,
+									"h": 218,
+									"w": 218
+								}
+							]
+						},
+						"small": {
+							"faces": [
+								{
+									"x": 272,
+									"y": 75,
+									"h": 185,
+									"w": 185
+								}
+							]
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1206505358157586400,
+					"id_str": "1206505358157586432",
+					"indices": [
+						122,
+						145
+					],
+					"media_url": "http://pbs.twimg.com/media/EL5eOxyWoAA29in.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EL5eOxyWoAA29in.jpg",
+					"url": "https://t.co/tb2xUQGNmd",
+					"display_url": "pic.twitter.com/tb2xUQGNmd",
+					"expanded_url": "https://twitter.com/OKCinfor/status/1206505359621402624/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 800,
+							"h": 533,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 800,
+							"h": 533,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"medium": {
+							"faces": [
+								{
+									"x": 320,
+									"y": 89,
+									"h": 218,
+									"w": 218
+								}
+							]
+						},
+						"orig": {
+							"faces": [
+								{
+									"x": 320,
+									"y": 89,
+									"h": 218,
+									"w": 218
+								}
+							]
+						},
+						"large": {
+							"faces": [
+								{
+									"x": 320,
+									"y": 89,
+									"h": 218,
+									"w": 218
+								}
+							]
+						},
+						"small": {
+							"faces": [
+								{
+									"x": 272,
+									"y": 75,
+									"h": 185,
+									"w": 185
+								}
+							]
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twibble.io\" rel=\"nofollow\">Twibble.io</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1097520031955587100,
+			"id_str": "1097520031955587073",
+			"name": "OKCinfo",
+			"screen_name": "OKCinfor",
+			"location": "Brussel, België",
+			"description": "An initiative to restore the truths against the toxic OKC Cult where physical, sexual & mental abuses enfolded for decades. Trial & Appeal ongoing check website",
+			"url": "https://t.co/feER16PibH",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/feER16PibH",
+							"expanded_url": "https://www.okcinfo.news",
+							"display_url": "okcinfo.news",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 50,
+			"friends_count": 242,
+			"listed_count": 1,
+			"created_at": "Mon Feb 18 15:35:41 +0000 2019",
+			"favourites_count": 63,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 273,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1140749925715775488/-ruh0TMj_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1140749925715775488/-ruh0TMj_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1097520031955587073/1562445469",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": true,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 5,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ht",
+		"text": "Tibetan Nyigma-Pa support sexual predators : Takloung Matrul Rinpotché à Bxl https://t.co/BS6TSP3DtT via @ogyenkun #metoo https://t.co/tb2xUQGNmd"
+	}
+]
