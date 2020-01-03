d067316987c899a6a31e5e208046f13d4ccf2b1c
Ben Borges <email@benborges.xyz>
2020-01-03 09:18:12 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 879,
-	"created_at": "Thu Jan 02 08:18:11 +0000 2020",
+	"tweets": 880,
+	"created_at": "Fri Jan 03 08:18:11 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_01.js
+++ data/js/tweets/2020_01.js
@@ -1,6 +1,216 @@
 Grailbird.data.tweets_2020_01 = 
 [
 	{
+		"created_at": "Thu Jan 02 12:29:19 +0000 2020",
+		"id": 1212712473788371000,
+		"id_str": "1212712473788370945",
+		"full_text": "The past and perhaps the present : Sexual &amp; Physical abuses in Tibetan Buddhism centers with a \"Vajrayana\" curriculum.\n\nthe Future : Your AUDIO RECORDED promise in front of 4 abuse survivors to organise an international meeting about sexual abuse in Tibetan Buddhism.\n\nthe result? https://t.co/c51k6y6kiA",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			284
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/c51k6y6kiA",
+					"expanded_url": "https://twitter.com/DalaiLama/status/1212323848177115137",
+					"display_url": "twitter.com/DalaiLama/stat…",
+					"indices": [
+						285,
+						308
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3309353195,
+			"id_str": "3309353195",
+			"name": "#OKCinfo",
+			"screen_name": "OKCinformation",
+			"location": "Belgium",
+			"description": "#OKCinfo is a 2015 initiative from ex-OKC born kids now civil parties to drag Ogyen Kunzang Choling/Robert Spatz @ogyenkun to Justice | FF @okcinfor",
+			"url": "https://t.co/7CidVABWxf",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/7CidVABWxf",
+							"expanded_url": "http://www.okcinfo.news/archives",
+							"display_url": "okcinfo.news/archives",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 150,
+			"friends_count": 353,
+			"listed_count": 8,
+			"created_at": "Fri Jun 05 15:00:16 +0000 2015",
+			"favourites_count": 261,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 876,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/656530168312262656/Ujovmh2w_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/656530168312262656/Ujovmh2w_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3309353195/1526300811",
+			"profile_link_color": "DD2E44",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1212323848177115100,
+		"quoted_status_id_str": "1212323848177115137",
+		"quoted_status_permalink": {
+			"url": "https://t.co/c51k6y6kiA",
+			"expanded": "https://twitter.com/DalaiLama/status/1212323848177115137",
+			"display": "twitter.com/DalaiLama/stat…"
+		},
+		"quoted_status": {
+			"created_at": "Wed Jan 01 10:45:03 +0000 2020",
+			"id": 1212323848177115100,
+			"id_str": "1212323848177115137",
+			"full_text": "Time’s always moving on. Nothing can stop it. The question is whether we use our time well or not. We can't do anything about the past, but what happens in the future depends on what we do now. We can create a happier future by remembering that in being human we are all the same.",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				280
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": []
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 20609518,
+				"id_str": "20609518",
+				"name": "Dalai Lama",
+				"screen_name": "DalaiLama",
+				"location": "Dharamsala, India",
+				"description": "Welcome to the official twitter page of the Office of His Holiness the 14th Dalai Lama.",
+				"url": "http://t.co/AKctyfx8On",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "http://t.co/AKctyfx8On",
+								"expanded_url": "http://dalailama.com",
+								"display_url": "dalailama.com",
+								"indices": [
+									0,
+									22
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 19303882,
+				"friends_count": 0,
+				"listed_count": 80452,
+				"created_at": "Wed Feb 11 18:34:22 +0000 2009",
+				"favourites_count": 1,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": true,
+				"statuses_count": 1529,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": true,
+				"profile_background_color": "FFD21D",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/529214699041067008/fqPBAr5s_normal.jpeg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/529214699041067008/fqPBAr5s_normal.jpeg",
+				"profile_link_color": "7D1900",
+				"profile_sidebar_border_color": "E8E3CE",
+				"profile_sidebar_fill_color": "F7F2DC",
+				"profile_text_color": "000000",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"can_media_tag": true,
+				"followed_by": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 31928,
+			"favorite_count": 96810,
+			"favorited": false,
+			"retweeted": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "The past and perhaps the present : Sexual &amp; Physical abuses in Tibetan Buddhism centers with a \"Vajrayana\" curriculum.\n\nthe Future : Your AUDIO RECORDED promise in front of 4 abuse survivors to organise an international meeting about sexual abuse in Tibetan Buddhism.\n\nthe result? https://t.co/c51k6y6kiA"
+	},
+	{
 		"created_at": "Wed Jan 01 12:48:55 +0000 2020",
 		"id": 1212351425692475400,
 		"id_str": "1212355018969550848",
