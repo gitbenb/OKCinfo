dd1d7aa7325d3c5c65c5a9ebb6c45ecb4e02455a
Ben Borges <email@benborges.xyz>
2020-01-27 09:18:13 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 918,
-	"created_at": "Sun Jan 26 08:18:12 +0000 2020",
+	"tweets": 919,
+	"created_at": "Mon Jan 27 08:18:12 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_01.js
+++ data/js/tweets/2020_01.js
@@ -1,6 +1,232 @@
 Grailbird.data.tweets_2020_01 = 
 [
 	{
+		"created_at": "Sun Jan 26 14:41:46 +0000 2020",
+		"id": 1221441972948349000,
+		"id_str": "1221443115002802177",
+		"full_text": "On the 20th February, we're back in Court, we hope to catch the bad guys &amp; get recognized for what happened to us, we're 23 cult-born kids today adult and free, representing many more...\nWe need your help to pay for our lawyers, wish us luck #crowdfunding #Justice #MeToo https://t.co/Kh9bcoEWNL",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			275
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "crowdfunding",
+					"indices": [
+						246,
+						259
+					]
+				},
+				{
+					"text": "Justice",
+					"indices": [
+						260,
+						268
+					]
+				},
+				{
+					"text": "MeToo",
+					"indices": [
+						269,
+						275
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1221441970121400300,
+					"id_str": "1221441970121400320",
+					"indices": [
+						276,
+						299
+					],
+					"media_url": "http://pbs.twimg.com/media/EPNvABbW4AAY9c7.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EPNvABbW4AAY9c7.jpg",
+					"url": "https://t.co/Kh9bcoEWNL",
+					"display_url": "pic.twitter.com/Kh9bcoEWNL",
+					"expanded_url": "https://twitter.com/benborges_/status/1221441972948348928/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 768,
+							"h": 512,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 768,
+							"h": 512,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"large": {
+							"faces": []
+						},
+						"orig": {
+							"faces": []
+						},
+						"small": {
+							"faces": []
+						},
+						"medium": {
+							"faces": []
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1221441970121400300,
+					"id_str": "1221441970121400320",
+					"indices": [
+						276,
+						299
+					],
+					"media_url": "http://pbs.twimg.com/media/EPNvABbW4AAY9c7.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EPNvABbW4AAY9c7.jpg",
+					"url": "https://t.co/Kh9bcoEWNL",
+					"display_url": "pic.twitter.com/Kh9bcoEWNL",
+					"expanded_url": "https://twitter.com/benborges_/status/1221441972948348928/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 768,
+							"h": 512,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 768,
+							"h": 512,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"large": {
+							"faces": []
+						},
+						"orig": {
+							"faces": []
+						},
+						"small": {
+							"faces": []
+						},
+						"medium": {
+							"faces": []
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 6590,
+			"id_str": "6590",
+			"name": "BenB",
+			"screen_name": "benborges_",
+			"location": "https://keybase.io/benborges",
+			"description": "OKC/Spatz cult Survivor | info activist, agitator, disruption of social injustice | focus : Protests, Far-Right, geopol, disinfo\nnot all RT are endorsements.",
+			"url": "https://t.co/brLVXiDBoA",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/brLVXiDBoA",
+							"expanded_url": "https://www.benborges.xyz/",
+							"display_url": "benborges.xyz",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3397,
+			"friends_count": 4999,
+			"listed_count": 352,
+			"created_at": "Thu Sep 21 18:27:47 +0000 2006",
+			"favourites_count": 5715,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 141744,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1A1B1F",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1135311617867026434/JUFJtNIZ_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1135311617867026434/JUFJtNIZ_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/6590/1539614465",
+			"profile_link_color": "FF0000",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "252429",
+			"profile_text_color": "666666",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": true,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 5,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "On the 20th February, we're back in Court, we hope to catch the bad guys &amp; get recognized for what happened to us, we're 23 cult-born kids today adult and free, representing many more...\nWe need your help to pay for our lawyers, wish us luck #crowdfunding #Justice #MeToo https://t.co/Kh9bcoEWNL"
+	},
+	{
 		"created_at": "Sat Jan 25 13:34:33 +0000 2020",
 		"id": 1221063811253330000,
 		"id_str": "1221063811253329922",
