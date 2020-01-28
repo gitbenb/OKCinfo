417ad5b1f8ebece7ada662d83c31f7e1568d8c6b
Ben Borges <email@benborges.xyz>
2020-01-28 09:18:13 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 919,
-	"created_at": "Mon Jan 27 08:18:12 +0000 2020",
+	"tweets": 920,
+	"created_at": "Tue Jan 28 08:18:12 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_01.js
+++ data/js/tweets/2020_01.js
@@ -1,6 +1,133 @@
 Grailbird.data.tweets_2020_01 = 
 [
 	{
+		"created_at": "Mon Jan 27 20:46:03 +0000 2020",
+		"id": 1221883322835431400,
+		"id_str": "1221897177590530048",
+		"full_text": "@benborges_  survived a #OKCinfo, a cult of abuse. \n\nThe leader is likely to manipulate the trial &amp; negate the reality of the victims. \n\nHelp them bring their leader to justice here: https://t.co/UjOxik2LAe\n\nThey're at €4,920/€10,000 of their goal.",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			252
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "OKCinfo",
+					"indices": [
+						24,
+						32
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "benborges_",
+					"name": "BenB",
+					"id": 6590,
+					"id_str": "6590",
+					"indices": [
+						0,
+						11
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/UjOxik2LAe",
+					"expanded_url": "https://www.okcinfo.news/",
+					"display_url": "okcinfo.news",
+					"indices": [
+						187,
+						210
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": 6590,
+		"in_reply_to_user_id_str": "6590",
+		"in_reply_to_screen_name": "benborges_",
+		"user": {
+			"id": 872491280932229100,
+			"id_str": "872491280932229123",
+			"name": "Samantha Kutner",
+			"screen_name": "ashkenaz89",
+			"location": "",
+			"description": "Dancer| #ProudBoys Whisperer| Research Consultant| in @natobserver @rollingstone | #CVE📍#OSINT 📍Info: samantha@proudboyswhisperer.com",
+			"url": "https://t.co/IQfy41U152",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/IQfy41U152",
+							"expanded_url": "https://www.patreon.com/ashkenaz89",
+							"display_url": "patreon.com/ashkenaz89",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3214,
+			"friends_count": 4979,
+			"listed_count": 38,
+			"created_at": "Wed Jun 07 16:31:46 +0000 2017",
+			"favourites_count": 22812,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 11273,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1203640897318948864/YSbErJw3_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1203640897318948864/YSbErJw3_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/872491280932229123/1579145833",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"can_media_tag": false,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@benborges_  survived a #OKCinfo, a cult of abuse. \n\nThe leader is likely to manipulate the trial &amp; negate the reality of the victims. \n\nHelp them bring their leader to justice here: https://t.co/UjOxik2LAe\n\nThey're at €4,920/€10,000 of their goal."
+	},
+	{
 		"created_at": "Sun Jan 26 14:41:46 +0000 2020",
 		"id": 1221441972948349000,
 		"id_str": "1221443115002802177",
