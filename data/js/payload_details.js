c6b4ba705051affbb58bc96578d9ee024c2ddce8
Ben Borges <email@benborges.xyz>
2020-02-27 09:18:12 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 942,
-	"created_at": "Sun Feb 23 08:18:11 +0000 2020",
+	"tweets": 943,
+	"created_at": "Thu Feb 27 08:18:11 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_02.js
+++ data/js/tweets/2020_02.js
@@ -1,6 +1,180 @@
 Grailbird.data.tweets_2020_02 = 
 [
 	{
+		"created_at": "Wed Feb 26 09:35:17 +0000 2020",
+		"id": 1232593111500689400,
+		"id_str": "1232600010245779456",
+		"full_text": "Ce matin, les audiences Témoins a décharge de la OKC continuent.\nun enfant de la dernière génération témoigne d'une période qui commence par les perquisitions de 1997 et se termine dans le courant des années 2000 lorsqu'il quitte Château de soleils pour Bruxelles.\ni https://t.co/oshNcw0TIT",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			266
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1232593104408055800,
+					"id_str": "1232593104408055808",
+					"indices": [
+						267,
+						290
+					],
+					"media_url": "http://pbs.twimg.com/media/ERsM5dzWkAAUBhF.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ERsM5dzWkAAUBhF.jpg",
+					"url": "https://t.co/oshNcw0TIT",
+					"display_url": "pic.twitter.com/oshNcw0TIT",
+					"expanded_url": "https://twitter.com/OKCinfor/status/1232593111500689408/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 720,
+							"h": 818,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 599,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 720,
+							"h": 818,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1232593104408055800,
+					"id_str": "1232593104408055808",
+					"indices": [
+						267,
+						290
+					],
+					"media_url": "http://pbs.twimg.com/media/ERsM5dzWkAAUBhF.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ERsM5dzWkAAUBhF.jpg",
+					"url": "https://t.co/oshNcw0TIT",
+					"display_url": "pic.twitter.com/oshNcw0TIT",
+					"expanded_url": "https://twitter.com/OKCinfor/status/1232593111500689408/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 720,
+							"h": 818,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 599,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 720,
+							"h": 818,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1097520031955587100,
+			"id_str": "1097520031955587073",
+			"name": "OKCinfo",
+			"screen_name": "OKCinfor",
+			"location": "Brussel, België",
+			"description": "Justice Initiative against Ogyen Kunzang Choling toxic Cult where physical, sexual abuses enfolded for decades. Trial 20th February 2020",
+			"url": "https://t.co/feER16PibH",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/feER16PibH",
+							"expanded_url": "https://www.okcinfo.news",
+							"display_url": "okcinfo.news",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 60,
+			"friends_count": 254,
+			"listed_count": 2,
+			"created_at": "Mon Feb 18 15:35:41 +0000 2019",
+			"favourites_count": 102,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 450,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1222871572010151936/Rn9iM3wB_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1222871572010151936/Rn9iM3wB_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1097520031955587073/1580395807",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "fr",
+		"text": "Ce matin, les audiences Témoins a décharge de la OKC continuent.\nun enfant de la dernière génération témoigne d'une période qui commence par les perquisitions de 1997 et se termine dans le courant des années 2000 lorsqu'il quitte Château de soleils pour Bruxelles.\ni https://t.co/oshNcw0TIT"
+	},
+	{
 		"created_at": "Sat Feb 22 14:01:58 +0000 2020",
 		"id": 1231217569610293200,
 		"id_str": "1231217569610293248",
