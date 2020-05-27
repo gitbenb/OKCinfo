632227bd6392e52fd1ec46bfd9dc29ca7d01c7fe
Ben Borges <email@benborges.xyz>
2020-05-27 10:18:12 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 952,
-	"created_at": "Sun May 10 08:18:11 +0000 2020",
+	"tweets": 953,
+	"created_at": "Wed May 27 08:18:11 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_05.js
+++ data/js/tweets/2020_05.js
@@ -1,6 +1,123 @@
 Grailbird.data.tweets_2020_05 = 
 [
 	{
+		"created_at": "Tue May 26 11:54:31 +0000 2020",
+		"id": 1265238818287992800,
+		"id_str": "1265249958074163201",
+		"full_text": "Soutenez l'initiative OKCinfo, si vous le pouvez, faites un don à notre crowdfunding, partagez ce lien, parlez de cette histoire autour de vous, nous avons besoin de votre soutien !… https://t.co/nzP9xcssDd via @okcinfor",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			220
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "OKCinfor",
+					"name": "OKCinfo",
+					"id": 1097520031955587100,
+					"id_str": "1097520031955587073",
+					"indices": [
+						211,
+						220
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/nzP9xcssDd",
+					"expanded_url": "https://okcinfo.news/b/2j",
+					"display_url": "okcinfo.news/b/2j",
+					"indices": [
+						183,
+						206
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1562961949,
+			"id_str": "1562961949",
+			"name": "Ricardo Mendes",
+			"screen_name": "rMdes_",
+			"location": "Brussels",
+			"description": "Tech Ninja against Covid19 | Info Aggregation, Monitoring Disinformation,Veille |\nTwitter bots 4 Good | Interest in Politics, Technology #Digital #SocEnt",
+			"url": "https://t.co/JdAD1V5t8X",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/JdAD1V5t8X",
+							"expanded_url": "https://www.rmendes.net/",
+							"display_url": "rmendes.net",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2271,
+			"friends_count": 4812,
+			"listed_count": 321,
+			"created_at": "Tue Jul 02 11:30:45 +0000 2013",
+			"favourites_count": 12481,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 36505,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1254135011546103815/1T8pdvoY_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1254135011546103815/1T8pdvoY_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1562961949/1587849688",
+			"profile_link_color": "3B94D9",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 5,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "fr",
+		"text": "Soutenez l'initiative OKCinfo, si vous le pouvez, faites un don à notre crowdfunding, partagez ce lien, parlez de cette histoire autour de vous, nous avons besoin de votre soutien !… https://t.co/nzP9xcssDd via @okcinfor"
+	},
+	{
 		"created_at": "Sat May 09 16:49:36 +0000 2020",
 		"id": 1259163622309335000,
 		"id_str": "1259163622309335040",
