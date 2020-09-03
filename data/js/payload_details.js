931e032ee289e1b1f6c494c18221b225c35e4900
Ben Borges <email@benborges.xyz>
2020-09-03 10:18:13 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 958,
-	"created_at": "Mon Aug 31 08:18:12 +0000 2020",
+	"tweets": 959,
+	"created_at": "Thu Sep 03 08:18:12 +0000 2020",
 	"lang": "en"
 }
---
+++ data/js/tweets/2020_09.js
@@ -0,0 +1,189 @@
+Grailbird.data.tweets_2020_09 = 
+[
+	{
+		"created_at": "Wed Sep 02 13:16:20 +0000 2020",
+		"id": 1301146946460094500,
+		"id_str": "1301146946460094471",
+		"full_text": "Vendredi 11/09 : plaidoiries d’OKC \nLundi 14/09 : plaidoiries pour Jeanne B et Olga de S\nMardi 15/09 jusqu’au jeudi 17/09 : plaidoiries pour Robert S \n\nAudience publique à Liège\ncontact @rMdes_ \n\n@BelgaNewsAgency @lalibrebe @lesoir @lecho @ladh @BelRTL @RTBFinfo @BX1Officiel",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			275
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "rMdes_",
+					"name": "Ricardo Mendes",
+					"id": 1562961949,
+					"id_str": "1562961949",
+					"indices": [
+						186,
+						193
+					]
+				},
+				{
+					"screen_name": "BelgaNewsAgency",
+					"name": "Belga News Agency",
+					"id": 1424934056,
+					"id_str": "1424934056",
+					"indices": [
+						196,
+						212
+					]
+				},
+				{
+					"screen_name": "lalibrebe",
+					"name": "lalibre.be",
+					"id": 20043573,
+					"id_str": "20043573",
+					"indices": [
+						213,
+						223
+					]
+				},
+				{
+					"screen_name": "lesoir",
+					"name": "Le Soir",
+					"id": 17169453,
+					"id_str": "17169453",
+					"indices": [
+						224,
+						231
+					]
+				},
+				{
+					"screen_name": "lecho",
+					"name": "L'Echo",
+					"id": 7682502,
+					"id_str": "7682502",
+					"indices": [
+						232,
+						238
+					]
+				},
+				{
+					"screen_name": "ladh",
+					"name": "DH les Sports +",
+					"id": 14715075,
+					"id_str": "14715075",
+					"indices": [
+						239,
+						244
+					]
+				},
+				{
+					"screen_name": "BelRTL",
+					"name": "Bel RTL",
+					"id": 231416367,
+					"id_str": "231416367",
+					"indices": [
+						245,
+						252
+					]
+				},
+				{
+					"screen_name": "RTBFinfo",
+					"name": "RTBF info",
+					"id": 14163474,
+					"id_str": "14163474",
+					"indices": [
+						253,
+						262
+					]
+				},
+				{
+					"screen_name": "BX1Officiel",
+					"name": "BX1Officiel",
+					"id": 558644855,
+					"id_str": "558644855",
+					"indices": [
+						263,
+						275
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1301146945097027600,
+		"in_reply_to_status_id_str": "1301146945097027585",
+		"in_reply_to_user_id": 3309353195,
+		"in_reply_to_user_id_str": "3309353195",
+		"in_reply_to_screen_name": "OKCinformation",
+		"user": {
+			"id": 3309353195,
+			"id_str": "3309353195",
+			"name": "OKCinfo Research",
+			"screen_name": "OKCinformation",
+			"location": "Belgique",
+			"description": "#OKCinfo is a 2015 initiative from ex-OKC born kids now civil parties to drag Ogyen Kunzang Choling/Robert Spatz @ogyenkun to Justice | FF @okcinfor",
+			"url": "https://t.co/gJaUqhElyi",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/gJaUqhElyi",
+							"expanded_url": "https://okcinfo.news/en",
+							"display_url": "okcinfo.news/en",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 151,
+			"friends_count": 347,
+			"listed_count": 6,
+			"created_at": "Fri Jun 05 15:00:16 +0000 2015",
+			"favourites_count": 269,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 976,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/656530168312262656/Ujovmh2w_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/656530168312262656/Ujovmh2w_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3309353195/1526300811",
+			"profile_link_color": "DD2E44",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 4,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "fr",
+		"text": "Vendredi 11/09 : plaidoiries d’OKC \nLundi 14/09 : plaidoiries pour Jeanne B et Olga de S\nMardi 15/09 jusqu’au jeudi 17/09 : plaidoiries pour Robert S \n\nAudience publique à Liège\ncontact @rMdes_ \n\n@BelgaNewsAgency @lalibrebe @lesoir @lecho @ladh @BelRTL @RTBFinfo @BX1Officiel"
+	}
+]
