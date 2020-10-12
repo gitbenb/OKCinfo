f59f3bf6388e287b88869a8c44386bf70381c1d4
Ben Borges <email@benborges.xyz>
2020-10-12 10:18:14 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 1025,
-	"created_at": "Fri Oct 02 08:18:12 +0000 2020",
+	"tweets": 1026,
+	"created_at": "Mon Oct 12 08:18:12 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_10.js
+++ data/js/tweets/2020_10.js
@@ -1,6 +1,112 @@
 Grailbird.data.tweets_2020_10 = 
 [
 	{
+		"created_at": "Sun Oct 11 09:01:00 +0000 2020",
+		"id": 1315215748151816200,
+		"id_str": "1315215813008404480",
+		"full_text": "After 20 years of proceedings, 5 years of trial, appeal, counter appeal, new trial, 8 months of trial in the midst of Covid-19, I have the pleasure to announce Judgement Day on December 2nd 2020\n\nA pedocriminal posing as Tibetan Buddhist Lama. \n\n https://t.co/c1uo7yaAn2",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			270
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/c1uo7yaAn2",
+					"expanded_url": "https://okcinfo.news/",
+					"display_url": "okcinfo.news",
+					"indices": [
+						247,
+						270
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 6590,
+			"id_str": "6590",
+			"name": "CyberBenB",
+			"screen_name": "cyberbenb",
+			"location": "🖤💛",
+			"description": "He/They - OKC/Spatz cult Survivor | info activist, agitator, disruption of social injustice | focus : Protests, Far-Right, geopol, Disinfo, Human Rights",
+			"url": "https://t.co/J3Ujnvrv6m",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/J3Ujnvrv6m",
+							"expanded_url": "https://benborges.xyz",
+							"display_url": "benborges.xyz",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3426,
+			"friends_count": 4581,
+			"listed_count": 321,
+			"created_at": "Thu Sep 21 18:27:47 +0000 2006",
+			"favourites_count": 6405,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 156957,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1A1B1F",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1267055358822023168/pjIpcmd8_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1267055358822023168/pjIpcmd8_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/6590/1602412667",
+			"profile_link_color": "FF0000",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "252429",
+			"profile_text_color": "666666",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "After 20 years of proceedings, 5 years of trial, appeal, counter appeal, new trial, 8 months of trial in the midst of Covid-19, I have the pleasure to announce Judgement Day on December 2nd 2020\n\nA pedocriminal posing as Tibetan Buddhist Lama. \n\n https://t.co/c1uo7yaAn2"
+	},
+	{
 		"created_at": "Thu Oct 01 13:26:03 +0000 2020",
 		"id": 1311658121367036000,
 		"id_str": "1311658638537359360",
