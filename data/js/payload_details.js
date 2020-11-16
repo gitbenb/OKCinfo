594f0584f6c6d791a371d1a40578e886fce87df9
Ben Borges <email@benborges.xyz>
2020-11-16 09:18:13 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 1030,
-	"created_at": "Thu Oct 29 08:18:12 +0000 2020",
+	"tweets": 1031,
+	"created_at": "Mon Nov 16 08:18:11 +0000 2020",
 	"lang": "en"
 }
---
+++ data/js/tweets/2020_11.js
@@ -0,0 +1,152 @@
+Grailbird.data.tweets_2020_11 = 
+[
+	{
+		"created_at": "Sun Nov 15 09:32:06 +0000 2020",
+		"id": 1327907215487938600,
+		"id_str": "1327907215487938560",
+		"full_text": "Jugement affaire OKC-Spatz \n#liege #justice #proces #cassation #pedocriminel #metoo \n\nhttps://t.co/HcIac8ceSf",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			109
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "liege",
+					"indices": [
+						28,
+						34
+					]
+				},
+				{
+					"text": "justice",
+					"indices": [
+						35,
+						43
+					]
+				},
+				{
+					"text": "proces",
+					"indices": [
+						44,
+						51
+					]
+				},
+				{
+					"text": "cassation",
+					"indices": [
+						52,
+						62
+					]
+				},
+				{
+					"text": "pedocriminel",
+					"indices": [
+						63,
+						76
+					]
+				},
+				{
+					"text": "metoo",
+					"indices": [
+						77,
+						83
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/HcIac8ceSf",
+					"expanded_url": "https://mobilizon.be/events/0dffd59f-7829-4724-b825-1d7c3f14f119",
+					"display_url": "mobilizon.be/events/0dffd59…",
+					"indices": [
+						86,
+						109
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3309353195,
+			"id_str": "3309353195",
+			"name": "OKCinfo Research",
+			"screen_name": "OKCinformation",
+			"location": "Belgique",
+			"description": "#OKCinfo is a 2015 initiative from ex-OKC born kids now civil parties to drag Ogyen Kunzang Choling/Robert Spatz @ogyenkun to Justice | contact @rmdes_",
+			"url": "https://t.co/j2r7QQC4g2",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/j2r7QQC4g2",
+							"expanded_url": "https://okcinfo.news/",
+							"display_url": "okcinfo.news",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 158,
+			"friends_count": 355,
+			"listed_count": 5,
+			"created_at": "Fri Jun 05 15:00:16 +0000 2015",
+			"favourites_count": 284,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1059,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/656530168312262656/Ujovmh2w_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/656530168312262656/Ujovmh2w_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3309353195/1600261840",
+			"profile_link_color": "DD2E44",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Jugement affaire OKC-Spatz \n#liege #justice #proces #cassation #pedocriminel #metoo \n\nhttps://t.co/HcIac8ceSf"
+	}
+]
