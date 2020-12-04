13ba728de56fdb7d0852db1ffcbcb4eca8ab88c7
Ben Borges <email@benborges.xyz>
2020-12-04 09:18:13 +0100

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 1034,
-	"created_at": "Thu Dec 03 08:18:11 +0000 2020",
+	"tweets": 1036,
+	"created_at": "Fri Dec 04 08:18:11 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_12.js
+++ data/js/tweets/2020_12.js
@@ -1,6 +1,495 @@
 Grailbird.data.tweets_2020_12 = 
 [
 	{
+		"created_at": "Thu Dec 03 20:08:08 +0000 2020",
+		"id": 1334590262375575600,
+		"id_str": "1334590262375575552",
+		"full_text": "Thank you All for your support these last 5 years❤️\nShort update while we craft another's longer update on our website. #okcinfo https://t.co/D93V2HdAuR @gofundme https://t.co/0g9gFLQnaV",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			162
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "okcinfo",
+					"indices": [
+						120,
+						128
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "gofundme",
+					"name": "GoFundMe",
+					"id": 112897540,
+					"id_str": "112897540",
+					"indices": [
+						153,
+						162
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/D93V2HdAuR",
+					"expanded_url": "https://www.gofundme.com/f/justice-for-okcspatz-survivors?sharetype=teams&member=3396598&utm_medium=social&utm_source=twitter&utm_campaign=p_na+share-sheet&pc_code=tw_co_dashboard_a&rcid=022d9551aa664edcbdf973623e451430",
+					"display_url": "gofundme.com/f/justice-for-…",
+					"indices": [
+						129,
+						152
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1334590259670212600,
+					"id_str": "1334590259670212609",
+					"indices": [
+						163,
+						186
+					],
+					"media_url": "http://pbs.twimg.com/media/EoVqxSxW4AELnb-.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EoVqxSxW4AELnb-.jpg",
+					"url": "https://t.co/0g9gFLQnaV",
+					"display_url": "pic.twitter.com/0g9gFLQnaV",
+					"expanded_url": "https://twitter.com/OKCinformation/status/1334590262375575552/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 768,
+							"h": 512,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 768,
+							"h": 512,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1334590259670212600,
+					"id_str": "1334590259670212609",
+					"indices": [
+						163,
+						186
+					],
+					"media_url": "http://pbs.twimg.com/media/EoVqxSxW4AELnb-.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EoVqxSxW4AELnb-.jpg",
+					"url": "https://t.co/0g9gFLQnaV",
+					"display_url": "pic.twitter.com/0g9gFLQnaV",
+					"expanded_url": "https://twitter.com/OKCinformation/status/1334590262375575552/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 768,
+							"h": 512,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 768,
+							"h": 512,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3309353195,
+			"id_str": "3309353195",
+			"name": "OKCinfo Research",
+			"screen_name": "OKCinformation",
+			"location": "Belgique",
+			"description": "#OKCinfo is a 2015 initiative from ex-OKC born kids now civil parties to drag Ogyen Kunzang Choling/Robert Spatz @ogyenkun to Justice | contact @rmdes_",
+			"url": "https://t.co/j2r7QQC4g2",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/j2r7QQC4g2",
+							"expanded_url": "https://okcinfo.news/",
+							"display_url": "okcinfo.news",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 160,
+			"friends_count": 356,
+			"listed_count": 5,
+			"created_at": "Fri Jun 05 15:00:16 +0000 2015",
+			"favourites_count": 285,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1064,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/656530168312262656/Ujovmh2w_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/656530168312262656/Ujovmh2w_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3309353195/1600261840",
+			"profile_link_color": "DD2E44",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Thank you All for your support these last 5 years❤️\nShort update while we craft another's longer update on our website. #okcinfo https://t.co/D93V2HdAuR @gofundme https://t.co/0g9gFLQnaV"
+	},
+	{
+		"created_at": "Thu Dec 03 16:56:34 +0000 2020",
+		"id": 1334531123049861000,
+		"id_str": "1334542052244254721",
+		"full_text": "Un jugement par la cour de Liège d'une précision et d'une motivation sans faille, un travail remarquable, de fond, sur les profondeurs de ce dossier vieux de 20 ans, une compréhension de nos vécus de parties civiles d'une rare objectivité et surtout précision. Reconnaissance. https://t.co/wfBvVKjunF",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			276
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/wfBvVKjunF",
+					"expanded_url": "https://twitter.com/rMdes_/status/1333452876073590785",
+					"display_url": "twitter.com/rMdes_/status/…",
+					"indices": [
+						277,
+						300
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1562961949,
+			"id_str": "1562961949",
+			"name": "Ricardo",
+			"screen_name": "rMdes_",
+			"location": "Brussels",
+			"description": "Metamorphosis, Learning to code | Building Autonomy & resilience | Interest in Politics, Technology, Democracy, Human Rights @PolBegov @EUwatchers FR/EN/PT",
+			"url": "https://t.co/8F9N2ddWth",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/8F9N2ddWth",
+							"expanded_url": "https://rmendes.net/",
+							"display_url": "rmendes.net",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2326,
+			"friends_count": 5000,
+			"listed_count": 319,
+			"created_at": "Tue Jul 02 11:30:45 +0000 2013",
+			"favourites_count": 13510,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 39172,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1309950691247566849/ism8yLjj_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1309950691247566849/ism8yLjj_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1562961949/1601151587",
+			"profile_link_color": "3B94D9",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1333452876073590800,
+		"quoted_status_id_str": "1333452876073590785",
+		"quoted_status_permalink": {
+			"url": "https://t.co/wfBvVKjunF",
+			"expanded": "https://twitter.com/rMdes_/status/1333452876073590785",
+			"display": "twitter.com/rMdes_/status/…"
+		},
+		"quoted_status": {
+			"created_at": "Mon Nov 30 16:48:34 +0000 2020",
+			"id": 1333452876073590800,
+			"id_str": "1333452876073590785",
+			"full_text": "Jugement Procès OKC-SPATZ\n2 décembre\n14h\nLiège\n\nDepuis 5 ans, des dizaines de ces enfants, aujourd'hui adultes, libre depuis entre 10 et 20 ans, attendent ce jugement, certaine partie civile depuis 1997\n\nMême si la défense/adeptes de OKC-Spatz aura passé beaucoup de temps https://t.co/wvWSjDwBt3",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				272
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1333452869991796700,
+						"id_str": "1333452869991796740",
+						"indices": [
+							273,
+							296
+						],
+						"media_url": "http://pbs.twimg.com/media/EoFgUgCWEAQRMpE.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EoFgUgCWEAQRMpE.jpg",
+						"url": "https://t.co/wvWSjDwBt3",
+						"display_url": "pic.twitter.com/wvWSjDwBt3",
+						"expanded_url": "https://twitter.com/rMdes_/status/1333452876073590785/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 861,
+								"h": 524,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 414,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 861,
+								"h": 524,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1333452869991796700,
+						"id_str": "1333452869991796740",
+						"indices": [
+							273,
+							296
+						],
+						"media_url": "http://pbs.twimg.com/media/EoFgUgCWEAQRMpE.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EoFgUgCWEAQRMpE.jpg",
+						"url": "https://t.co/wvWSjDwBt3",
+						"display_url": "pic.twitter.com/wvWSjDwBt3",
+						"expanded_url": "https://twitter.com/rMdes_/status/1333452876073590785/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 861,
+								"h": 524,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 414,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 861,
+								"h": 524,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 1562961949,
+				"id_str": "1562961949",
+				"name": "Ricardo",
+				"screen_name": "rMdes_",
+				"location": "Brussels",
+				"description": "Metamorphosis, Learning to code | Building Autonomy & resilience | Interest in Politics, Technology, Democracy, Human Rights @PolBegov @EUwatchers FR/EN/PT",
+				"url": "https://t.co/8F9N2ddWth",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/8F9N2ddWth",
+								"expanded_url": "https://rmendes.net/",
+								"display_url": "rmendes.net",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 2326,
+				"friends_count": 5000,
+				"listed_count": 319,
+				"created_at": "Tue Jul 02 11:30:45 +0000 2013",
+				"favourites_count": 13510,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 39172,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1309950691247566849/ism8yLjj_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1309950691247566849/ism8yLjj_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/1562961949/1601151587",
+				"profile_link_color": "3B94D9",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "000000",
+				"profile_use_background_image": false,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 2,
+			"favorite_count": 2,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "fr"
+		},
+		"retweet_count": 4,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "fr",
+		"text": "Un jugement par la cour de Liège d'une précision et d'une motivation sans faille, un travail remarquable, de fond, sur les profondeurs de ce dossier vieux de 20 ans, une compréhension de nos vécus de parties civiles d'une rare objectivité et surtout précision. Reconnaissance. https://t.co/wfBvVKjunF"
+	},
+	{
 		"created_at": "Wed Dec 02 09:36:47 +0000 2020",
 		"id": 1334068989560049700,
 		"id_str": "1334068989560049669",
