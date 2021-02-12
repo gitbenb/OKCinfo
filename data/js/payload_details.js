cd551ca6feb59d64e8f0c7fffeeb36956bee8acf
Ben Borges <email@benborges.xyz>
2021-02-12 09:18:12 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 1086,
-	"created_at": "Thu Feb 11 08:18:11 +0000 2021",
+	"tweets": 1087,
+	"created_at": "Fri Feb 12 08:18:11 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_02.js
+++ data/js/tweets/2021_02.js
@@ -1,6 +1,239 @@
 Grailbird.data.tweets_2021_02 = 
 [
 	{
+		"created_at": "Fri Feb 12 08:01:48 +0000 2021",
+		"id": 1360137010837659600,
+		"id_str": "1360137010837659650",
+		"full_text": "Still banned, @facebook your feedback collection about specific banned URL is bullshit, doesn't work, it's useless and a black box that is impossible to appeal. #fixyourshit @facebookapp @fbnewsroom @FacebookWatch @fb_engineering https://t.co/bvAEllkxA3",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			229
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "fixyourshit",
+					"indices": [
+						161,
+						173
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Facebook",
+					"name": "Facebook",
+					"id": 2425151,
+					"id_str": "2425151",
+					"indices": [
+						14,
+						23
+					]
+				},
+				{
+					"screen_name": "facebookapp",
+					"name": "Facebook App",
+					"id": 1174426031010152400,
+					"id_str": "1174426031010152448",
+					"indices": [
+						174,
+						186
+					]
+				},
+				{
+					"screen_name": "fbnewsroom",
+					"name": "Facebook Newsroom",
+					"id": 163723677,
+					"id_str": "163723677",
+					"indices": [
+						187,
+						198
+					]
+				},
+				{
+					"screen_name": "FacebookWatch",
+					"name": "facebookwatch",
+					"id": 899733078641225700,
+					"id_str": "899733078641225728",
+					"indices": [
+						199,
+						213
+					]
+				},
+				{
+					"screen_name": "fb_engineering",
+					"name": "Facebook Engineering",
+					"id": 68639853,
+					"id_str": "68639853",
+					"indices": [
+						214,
+						229
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1360137007876477000,
+					"id_str": "1360137007876476928",
+					"indices": [
+						230,
+						253
+					],
+					"media_url": "http://pbs.twimg.com/media/EuAtZwBXEAA36Rc.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EuAtZwBXEAA36Rc.jpg",
+					"url": "https://t.co/bvAEllkxA3",
+					"display_url": "pic.twitter.com/bvAEllkxA3",
+					"expanded_url": "https://twitter.com/OKCinformation/status/1360137010837659650/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1072,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 628,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 356,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1360137007876477000,
+					"id_str": "1360137007876476928",
+					"indices": [
+						230,
+						253
+					],
+					"media_url": "http://pbs.twimg.com/media/EuAtZwBXEAA36Rc.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EuAtZwBXEAA36Rc.jpg",
+					"url": "https://t.co/bvAEllkxA3",
+					"display_url": "pic.twitter.com/bvAEllkxA3",
+					"expanded_url": "https://twitter.com/OKCinformation/status/1360137010837659650/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1072,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 628,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 356,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": 1359867973658378200,
+		"in_reply_to_status_id_str": "1359867973658378240",
+		"in_reply_to_user_id": 3309353195,
+		"in_reply_to_user_id_str": "3309353195",
+		"in_reply_to_screen_name": "OKCinformation",
+		"user": {
+			"id": 3309353195,
+			"id_str": "3309353195",
+			"name": "OKCinfo Justice initiative",
+			"screen_name": "OKCinformation",
+			"location": "Belgique",
+			"description": "#OKCinfo is a 2015 initiative from ex-OKC born kids now civil parties to drag Ogyen Kunzang Choling/Robert Spatz @ogyenkun to Justice | contact @rmdes_",
+			"url": "https://t.co/j2r7QQC4g2",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/j2r7QQC4g2",
+							"expanded_url": "https://okcinfo.news/",
+							"display_url": "okcinfo.news",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 164,
+			"friends_count": 359,
+			"listed_count": 7,
+			"created_at": "Fri Jun 05 15:00:16 +0000 2015",
+			"favourites_count": 305,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1127,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/656530168312262656/Ujovmh2w_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/656530168312262656/Ujovmh2w_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3309353195/1600261840",
+			"profile_link_color": "DD2E44",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Still banned, @facebook your feedback collection about specific banned URL is bullshit, doesn't work, it's useless and a black box that is impossible to appeal. #fixyourshit @facebookapp @fbnewsroom @FacebookWatch @fb_engineering https://t.co/bvAEllkxA3"
+	},
+	{
 		"created_at": "Thu Feb 11 00:43:02 +0000 2021",
 		"id": 1359664204630868000,
 		"id_str": "1359664204630867971",
