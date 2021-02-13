ddfa4bbbade3f17e36d62cd43a787bbe6e5637d7
Ben Borges <email@benborges.xyz>
2021-02-13 09:18:11 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 1087,
-	"created_at": "Fri Feb 12 08:18:11 +0000 2021",
+	"tweets": 1088,
+	"created_at": "Sat Feb 13 08:18:10 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_02.js
+++ data/js/tweets/2021_02.js
@@ -1,6 +1,148 @@
 Grailbird.data.tweets_2021_02 = 
 [
 	{
+		"created_at": "Fri Feb 12 12:28:48 +0000 2021",
+		"id": 1360204202992169000,
+		"id_str": "1360204202992168963",
+		"full_text": "Sophie, Ricardo et Philippe ont grandi dans la secte de Robert Spatz.\nIsolés du monde extérieur, ils y ont été victimes de nombreux abus et maltraitances. Aujourd'hui, ils témoignent. https://t.co/fcCgYHAIcN #okcinfo #sectes #dérivessectaires #bouddhismetibétain #spatzisme",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			273
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "okcinfo",
+					"indices": [
+						208,
+						216
+					]
+				},
+				{
+					"text": "sectes",
+					"indices": [
+						217,
+						224
+					]
+				},
+				{
+					"text": "dérivessectaires",
+					"indices": [
+						225,
+						242
+					]
+				},
+				{
+					"text": "bouddhismetibétain",
+					"indices": [
+						243,
+						262
+					]
+				},
+				{
+					"text": "spatzisme",
+					"indices": [
+						263,
+						273
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/fcCgYHAIcN",
+					"expanded_url": "https://www.youtube.com/watch?v=3HZtYY5e6y8",
+					"display_url": "youtube.com/watch?v=3HZtYY…",
+					"indices": [
+						184,
+						207
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3309353195,
+			"id_str": "3309353195",
+			"name": "OKCinfo Justice initiative",
+			"screen_name": "OKCinformation",
+			"location": "Belgique",
+			"description": "#OKCinfo is a 2015 initiative from ex-OKC born kids now civil parties to drag Ogyen Kunzang Choling/Robert Spatz @ogyenkun to Justice | contact @rmdes_",
+			"url": "https://t.co/j2r7QQC4g2",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/j2r7QQC4g2",
+							"expanded_url": "https://okcinfo.news/",
+							"display_url": "okcinfo.news",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 164,
+			"friends_count": 359,
+			"listed_count": 7,
+			"created_at": "Fri Jun 05 15:00:16 +0000 2015",
+			"favourites_count": 305,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1128,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/656530168312262656/Ujovmh2w_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/656530168312262656/Ujovmh2w_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3309353195/1600261840",
+			"profile_link_color": "DD2E44",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 4,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "fr",
+		"text": "Sophie, Ricardo et Philippe ont grandi dans la secte de Robert Spatz.\nIsolés du monde extérieur, ils y ont été victimes de nombreux abus et maltraitances. Aujourd'hui, ils témoignent. https://t.co/fcCgYHAIcN #okcinfo #sectes #dérivessectaires #bouddhismetibétain #spatzisme"
+	},
+	{
 		"created_at": "Fri Feb 12 08:01:48 +0000 2021",
 		"id": 1360137010837659600,
 		"id_str": "1360137010837659650",
