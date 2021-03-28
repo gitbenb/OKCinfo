70b8492460b2692449dec8d9ccc797475f09233f
Ben Borges <email@benborges.xyz>
2021-03-28 10:18:12 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 1107,
-	"created_at": "Fri Mar 26 08:18:11 +0000 2021",
+	"tweets": 1108,
+	"created_at": "Sun Mar 28 08:18:11 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_03.js
+++ data/js/tweets/2021_03.js
@@ -1,6 +1,231 @@
 Grailbird.data.tweets_2021_03 = 
 [
 	{
+		"created_at": "Sun Mar 28 06:43:33 +0000 2021",
+		"id": 1376062388068348000,
+		"id_str": "1376062388068347906",
+		"full_text": "After 5 years of trial, 2 Judgements, 2 appeals, we couldn't agree more with this, in fact, at some point, it was almost as if the Predators lawyers (certainly the one defending Spatz) was reading out loud theories, narratives and false arguments written by their client. https://t.co/mfC8pUVb8I",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			271
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/mfC8pUVb8I",
+					"expanded_url": "https://twitter.com/CCCBuryStEd/status/1375327322283327488",
+					"display_url": "twitter.com/CCCBuryStEd/st…",
+					"indices": [
+						272,
+						295
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3309353195,
+			"id_str": "3309353195",
+			"name": "OKCinfo Research",
+			"screen_name": "OKCinformation",
+			"location": "Belgique",
+			"description": "#OKCinfo is a 2015 Justice initiative from ex-OKC born kids now civil parties to drag Ogyen Kunzang Choling/Robert Spatz @ogyenkun to Justice | contact @rmdes_",
+			"url": "https://t.co/gJaUqhElyi",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/gJaUqhElyi",
+							"expanded_url": "https://okcinfo.news/en",
+							"display_url": "okcinfo.news/en",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 168,
+			"friends_count": 356,
+			"listed_count": 7,
+			"created_at": "Fri Jun 05 15:00:16 +0000 2015",
+			"favourites_count": 313,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1153,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/656530168312262656/Ujovmh2w_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/656530168312262656/Ujovmh2w_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3309353195/1614446992",
+			"profile_link_color": "DD2E44",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1375327322283327500,
+		"quoted_status_id_str": "1375327322283327488",
+		"quoted_status_permalink": {
+			"url": "https://t.co/mfC8pUVb8I",
+			"expanded": "https://twitter.com/CCCBuryStEd/status/1375327322283327488",
+			"display": "twitter.com/CCCBuryStEd/st…"
+		},
+		"quoted_status": {
+			"created_at": "Fri Mar 26 06:02:40 +0000 2021",
+			"id": 1375327322283327500,
+			"id_str": "1375327322283327488",
+			"full_text": "This is something I grappled with ALL THE TIME.\n\nI get that a lawyer will defend their client. Whoever the client is. \n\nMy issue is with the WAY the client is defended and the way their adversary is presented. It often replicates how an abuser behaves. https://t.co/B8lChZBTJX",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				252
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/B8lChZBTJX",
+						"expanded_url": "https://twitter.com/care_2_talk/status/1375173546381557761",
+						"display_url": "twitter.com/care_2_talk/st…",
+						"indices": [
+							253,
+							276
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/#!/download/ipad\" rel=\"nofollow\">Twitter for iPad</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 3327953062,
+				"id_str": "3327953062",
+				"name": "Coercive Control & Recovering From Trauma",
+				"screen_name": "CCCBuryStEd",
+				"location": "Bury St Edmunds, UK",
+				"description": "To properly understand coercive control, it is necessary to understand the emotional scars that are left - even when the relationship has ended.",
+				"url": "https://t.co/iJrVMcrUuH",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/iJrVMcrUuH",
+								"expanded_url": "http://coercivecontrol.co.uk",
+								"display_url": "coercivecontrol.co.uk",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 15354,
+				"friends_count": 4990,
+				"listed_count": 115,
+				"created_at": "Mon Jun 15 20:36:25 +0000 2015",
+				"favourites_count": 22257,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 64440,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/654735822722199552/SO5ZfsdE_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/654735822722199552/SO5ZfsdE_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/3327953062/1442895563",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": true,
+			"quoted_status_id": 1375173546381557800,
+			"quoted_status_id_str": "1375173546381557761",
+			"quoted_status_permalink": {
+				"url": "https://t.co/B8lChZBTJX",
+				"expanded": "https://twitter.com/care_2_talk/status/1375173546381557761",
+				"display": "twitter.com/care_2_talk/st…"
+			},
+			"retweet_count": 5,
+			"favorite_count": 38,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "After 5 years of trial, 2 Judgements, 2 appeals, we couldn't agree more with this, in fact, at some point, it was almost as if the Predators lawyers (certainly the one defending Spatz) was reading out loud theories, narratives and false arguments written by their client. https://t.co/mfC8pUVb8I"
+	},
+	{
 		"created_at": "Thu Mar 25 09:45:34 +0000 2021",
 		"id": 1375021029471481900,
 		"id_str": "1375021029471481857",
