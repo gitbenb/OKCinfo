a8bd571974f8ed0562559d98300d9b69d83fd24a
Ben Borges <email@benborges.xyz>
2021-05-15 10:18:12 +0200

    Added 1 tweets

--- data/js/tweets/2021_05.js
+++ data/js/tweets/2021_05.js
@@ -1,6 +1,124 @@
 Grailbird.data.tweets_2021_05 = 
 [
 	{
+		"created_at": "Fri May 14 11:22:30 +0000 2021",
+		"id": 1393156849705394200,
+		"id_str": "1393164816815099907",
+		"full_text": "Une enfance sous emprise, racontée par deux survivants de la dérive OKC-Spatz \n\nParfois pour terrasser l'ennemi, il faut contourner la montagne et attaquer sur deux fronts \n\nhttps://t.co/J6hf8EnM02 via @LouieMedia",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			213
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "LouieMedia",
+					"name": "Louie Media",
+					"id": 910479662899454000,
+					"id_str": "910479662899453952",
+					"indices": [
+						202,
+						213
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/J6hf8EnM02",
+					"expanded_url": "https://play.acast.com/s/a7277f6f-bb0c-49df-ac44-80cf3167a59b/beb5a4da-fb85-4cf8-a12c-a6c49cc1c1d6",
+					"display_url": "play.acast.com/s/a7277f6f-bb0…",
+					"indices": [
+						174,
+						197
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1562961949,
+			"id_str": "1562961949",
+			"name": "Ricardo",
+			"screen_name": "rMdes_",
+			"location": "Brussels",
+			"description": "Learning to code | Building Autonomy & resilience | Interest in Politics, Technology, Democracy, Human Rights @PolBegov @EUwatchers Spokesperson @OKCinfor",
+			"url": "https://t.co/zNucbsHeDX",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/zNucbsHeDX",
+							"expanded_url": "https://rmendes.net/",
+							"display_url": "rmendes.net",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2347,
+			"friends_count": 5001,
+			"listed_count": 318,
+			"created_at": "Tue Jul 02 11:30:45 +0000 2013",
+			"favourites_count": 13937,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 40535,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1358679817772818432/XBR5jRYz_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1358679817772818432/XBR5jRYz_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1562961949/1607346679",
+			"profile_link_color": "3B94D9",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "fr",
+		"text": "Une enfance sous emprise, racontée par deux survivants de la dérive OKC-Spatz \n\nParfois pour terrasser l'ennemi, il faut contourner la montagne et attaquer sur deux fronts \n\nhttps://t.co/J6hf8EnM02 via @LouieMedia"
+	},
+	{
 		"created_at": "Thu May 13 14:09:54 +0000 2021",
 		"id": 1392844207048515600,
 		"id_str": "1392844557474222086",
