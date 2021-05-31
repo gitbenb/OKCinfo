58fc3ce57672891892f1b4818b4e02658968cfcf
Ben Borges <email@benborges.xyz>
2021-05-31 10:18:13 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 1135,
-	"created_at": "Sun May 30 08:18:11 +0000 2021",
+	"tweets": 1136,
+	"created_at": "Mon May 31 08:18:12 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_05.js
+++ data/js/tweets/2021_05.js
@@ -1,6 +1,181 @@
 Grailbird.data.tweets_2021_05 = 
 [
 	{
+		"created_at": "Mon May 31 06:07:35 +0000 2021",
+		"id": 1399217997152198700,
+		"id_str": "1399246159844200448",
+		"full_text": "If, as a Buddhist, you are in a position of power and you don't use it to stop ongoing injustice, to stop a pedocriminal, than you are not worth your title, lineage, reputation, books and your supposed wisdom, you're just an enabler, in a red robe.\n\nThis is not a famous citation https://t.co/svxytbJTw1",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			279
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1399217994853716000,
+					"id_str": "1399217994853715970",
+					"indices": [
+						280,
+						303
+					],
+					"media_url": "http://pbs.twimg.com/media/E2sFWlqXEAICGjX.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E2sFWlqXEAICGjX.jpg",
+					"url": "https://t.co/svxytbJTw1",
+					"display_url": "pic.twitter.com/svxytbJTw1",
+					"expanded_url": "https://twitter.com/BuddhistLeaks/status/1399217997152198659/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 448,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 759,
+							"h": 500,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 759,
+							"h": 500,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1399217994853716000,
+					"id_str": "1399217994853715970",
+					"indices": [
+						280,
+						303
+					],
+					"media_url": "http://pbs.twimg.com/media/E2sFWlqXEAICGjX.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E2sFWlqXEAICGjX.jpg",
+					"url": "https://t.co/svxytbJTw1",
+					"display_url": "pic.twitter.com/svxytbJTw1",
+					"expanded_url": "https://twitter.com/BuddhistLeaks/status/1399217997152198659/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 448,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 759,
+							"h": 500,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 759,
+							"h": 500,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 890549322240196600,
+			"id_str": "890549322240196612",
+			"name": "Tibetan Buddhism Leaks",
+			"screen_name": "BuddhistLeaks",
+			"location": "Dharamsala",
+			"description": "Newsbot amplifying comments & news regarding Tibetan Buddhist sexual abuses allegations🔥FPMT, Shambhala, RIGPA, FWBO, NKT, Triratna, Ogyen Kunzang Choling",
+			"url": "https://t.co/ajelpwOAQm",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/ajelpwOAQm",
+							"expanded_url": "https://AbusesInTibetanBuddhism.info",
+							"display_url": "AbusesInTibetanBuddhism.info",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 397,
+			"friends_count": 1466,
+			"listed_count": 0,
+			"created_at": "Thu Jul 27 12:27:58 +0000 2017",
+			"favourites_count": 267,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 11183,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1340250942374105089/kAd0Uc3w_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1340250942374105089/kAd0Uc3w_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/890549322240196612/1608375674",
+			"profile_link_color": "FF691F",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 5,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "If, as a Buddhist, you are in a position of power and you don't use it to stop ongoing injustice, to stop a pedocriminal, than you are not worth your title, lineage, reputation, books and your supposed wisdom, you're just an enabler, in a red robe.\n\nThis is not a famous citation https://t.co/svxytbJTw1"
+	},
+	{
 		"created_at": "Sat May 29 09:06:13 +0000 2021",
 		"id": 1398565524670402600,
 		"id_str": "1398566337618849796",
