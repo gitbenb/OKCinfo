dbe0629b1447d1d1750b67582e8a843804000eb8
Ben Borges <email@benborges.xyz>
2021-10-30 10:18:13 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 1224,
-	"created_at": "Sat Oct 23 08:18:12 +0000 2021",
+	"tweets": 1225,
+	"created_at": "Sat Oct 30 08:18:12 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_10.js
+++ data/js/tweets/2021_10.js
@@ -1,6 +1,133 @@
 Grailbird.data.tweets_2021_10 = 
 [
 	{
+		"created_at": "Sat Oct 30 06:54:07 +0000 2021",
+		"id": 1453465018914283500,
+		"id_str": "1454340822569209859",
+		"full_text": "Lenteur de la Justice :\n\nL'affaire OKC-Spatz, perquisitions en 1997, 18 ans d'instruction, premier procès 2016, depuis 2 procès, 3 appels en cassation, dont le dernier appel du jugement du 2 décembre 2020 est toujours en cours.\n\nLes victimes:\n@OKCinfor \n\n@PhGoffin @SophieRohonyi",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			279
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "OKCinfor",
+					"name": "Initiative de Justice OKCinfo",
+					"id": 1097520031955587100,
+					"id_str": "1097520031955587073",
+					"indices": [
+						243,
+						252
+					]
+				},
+				{
+					"screen_name": "PhGoffin",
+					"name": "Philippe Goffin",
+					"id": 362914325,
+					"id_str": "362914325",
+					"indices": [
+						255,
+						264
+					]
+				},
+				{
+					"screen_name": "SophieRohonyi",
+					"name": "Sophie Rohonyi",
+					"id": 1851210716,
+					"id_str": "1851210716",
+					"indices": [
+						265,
+						279
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2359778089,
+			"id_str": "2359778089",
+			"name": "Veille infopol",
+			"screen_name": "PolBegov",
+			"location": "I block racists & xenophobes",
+			"description": "NewsBot Veille Info 🇧🇪\nAuto-Alimenté par les sections politique, économie et région des médias en Belgique - #ResetBelgium \n\n\n|\n\nOpinions ~@rMdes_",
+			"url": "https://t.co/BjehhGm8yv",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/BjehhGm8yv",
+							"expanded_url": "https://www.euwatch.live/polbegov-begov/",
+							"display_url": "euwatch.live/polbegov-begov/",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 5488,
+			"friends_count": 2661,
+			"listed_count": 250,
+			"created_at": "Mon Feb 24 16:54:12 +0000 2014",
+			"favourites_count": 6646,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 296853,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1A1B1F",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1366445232179605507/aumS2UHQ_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1366445232179605507/aumS2UHQ_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2359778089/1448275480",
+			"profile_link_color": "DD2E44",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "C0DFEC",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 4,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "fr",
+		"text": "Lenteur de la Justice :\n\nL'affaire OKC-Spatz, perquisitions en 1997, 18 ans d'instruction, premier procès 2016, depuis 2 procès, 3 appels en cassation, dont le dernier appel du jugement du 2 décembre 2020 est toujours en cours.\n\nLes victimes:\n@OKCinfor \n\n@PhGoffin @SophieRohonyi"
+	},
+	{
 		"created_at": "Fri Oct 22 10:19:30 +0000 2021",
 		"id": 1451491281201414100,
 		"id_str": "1451493409336799234",
