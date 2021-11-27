3a697831f13c29605266441b898e4390ccf57e7a
Ben Borges <email@benborges.xyz>
2021-11-27 09:18:12 +0100

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 1230,
-	"created_at": "Fri Nov 26 08:18:11 +0000 2021",
+	"tweets": 1232,
+	"created_at": "Sat Nov 27 08:18:11 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_11.js
+++ data/js/tweets/2021_11.js
@@ -1,6 +1,274 @@
 Grailbird.data.tweets_2021_11 = 
 [
 	{
+		"created_at": "Fri Nov 26 13:41:41 +0000 2021",
+		"id": 1464227733123342300,
+		"id_str": "1464227863146733594",
+		"full_text": "@BuddhistLeaks @OKCinformation @RTulku @ringutulku @PeljorTenzin @MatthieuRicard All anyone need do is look at what happened to Kalu Yangsi and the total lack of action by the kagyu establishment. If that level of abuse is happening to the hugest tulku in the monastery no need to wonder what happens to poor nepali boys or Tibetan refugees",
+		"truncated": false,
+		"display_text_range": [
+			81,
+			340
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "BuddhistLeaks",
+					"name": "Tibetan Buddhism Leaks",
+					"id": 890549322240196600,
+					"id_str": "890549322240196612",
+					"indices": [
+						0,
+						14
+					]
+				},
+				{
+					"screen_name": "OKCinformation",
+					"name": "OKCinfo Research",
+					"id": 3309353195,
+					"id_str": "3309353195",
+					"indices": [
+						15,
+						30
+					]
+				},
+				{
+					"screen_name": "RTulku",
+					"name": "Ringu Tulku",
+					"id": 475881098,
+					"id_str": "475881098",
+					"indices": [
+						31,
+						38
+					]
+				},
+				{
+					"screen_name": "ringutulku",
+					"name": "Ringu Tulku",
+					"id": 137360097,
+					"id_str": "137360097",
+					"indices": [
+						39,
+						50
+					]
+				},
+				{
+					"screen_name": "PeljorTenzin",
+					"name": "Tenzin Peljor",
+					"id": 2873081511,
+					"id_str": "2873081511",
+					"indices": [
+						51,
+						64
+					]
+				},
+				{
+					"screen_name": "MatthieuRicard",
+					"name": "Matthieu Ricard",
+					"id": 181228051,
+					"id_str": "181228051",
+					"indices": [
+						65,
+						80
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1464226371270549500,
+		"in_reply_to_status_id_str": "1464226371270549546",
+		"in_reply_to_user_id": 890549322240196600,
+		"in_reply_to_user_id_str": "890549322240196612",
+		"in_reply_to_screen_name": "BuddhistLeaks",
+		"user": {
+			"id": 2691156342,
+			"id_str": "2691156342",
+			"name": "Robert James",
+			"screen_name": "SatoriScience",
+			"location": "Montréal, Québec",
+			"description": "Marketing & Content @tristargym . Natural State Jiu Jitsu. Film Industry. Dzogchen Meditation.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 198,
+			"friends_count": 786,
+			"listed_count": 2,
+			"created_at": "Tue Jul 29 22:18:52 +0000 2014",
+			"favourites_count": 13491,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1445,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1378567427945074688/QkufxK81_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1378567427945074688/QkufxK81_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2691156342/1561731290",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "en",
+		"text": "@BuddhistLeaks @OKCinformation @RTulku @ringutulku @PeljorTenzin @MatthieuRicard All anyone need do is look at what happened to Kalu Yangsi and the total lack of action by the kagyu establishment. If that level of abuse is happening to the hugest tulku in the monastery no need to wonder what happens to poor nepali boys or Tibetan refugees"
+	},
+	{
+		"created_at": "Fri Nov 26 13:11:26 +0000 2021",
+		"id": 1464218040149680000,
+		"id_str": "1464220251642544131",
+		"full_text": "@OKCinformation @RTulku @ringutulku @PeljorTenzin I’ve spent the past ten year collecting first hand accounts of sexual abuse by Kagyu Tulkus, Khenpos and ordinary monks. It’s a pervasive and institutional problem and the majority of young Tulku I’ve asked have been abused themselves.",
+		"truncated": false,
+		"display_text_range": [
+			50,
+			285
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "OKCinformation",
+					"name": "OKCinfo Research",
+					"id": 3309353195,
+					"id_str": "3309353195",
+					"indices": [
+						0,
+						15
+					]
+				},
+				{
+					"screen_name": "RTulku",
+					"name": "Ringu Tulku",
+					"id": 475881098,
+					"id_str": "475881098",
+					"indices": [
+						16,
+						23
+					]
+				},
+				{
+					"screen_name": "ringutulku",
+					"name": "Ringu Tulku",
+					"id": 137360097,
+					"id_str": "137360097",
+					"indices": [
+						24,
+						35
+					]
+				},
+				{
+					"screen_name": "PeljorTenzin",
+					"name": "Tenzin Peljor",
+					"id": 2873081511,
+					"id_str": "2873081511",
+					"indices": [
+						36,
+						49
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1464207028616933400,
+		"in_reply_to_status_id_str": "1464207028616933377",
+		"in_reply_to_user_id": 2691156342,
+		"in_reply_to_user_id_str": "2691156342",
+		"in_reply_to_screen_name": "SatoriScience",
+		"user": {
+			"id": 2691156342,
+			"id_str": "2691156342",
+			"name": "Robert James",
+			"screen_name": "SatoriScience",
+			"location": "Montréal, Québec",
+			"description": "Marketing & Content @tristargym . Natural State Jiu Jitsu. Film Industry. Dzogchen Meditation.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 198,
+			"friends_count": 786,
+			"listed_count": 2,
+			"created_at": "Tue Jul 29 22:18:52 +0000 2014",
+			"favourites_count": 13491,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1445,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1378567427945074688/QkufxK81_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1378567427945074688/QkufxK81_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2691156342/1561731290",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 3,
+		"favorited": true,
+		"retweeted": true,
+		"lang": "en",
+		"text": "@OKCinformation @RTulku @ringutulku @PeljorTenzin I’ve spent the past ten year collecting first hand accounts of sexual abuse by Kagyu Tulkus, Khenpos and ordinary monks. It’s a pervasive and institutional problem and the majority of young Tulku I’ve asked have been abused themselves."
+	},
+	{
 		"created_at": "Fri Nov 26 00:14:17 +0000 2021",
 		"id": 1463798744021938200,
 		"id_str": "1464024676905197570",
