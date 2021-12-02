5c91db6c56ca1e5429ff5f56062009e1a8d5a74b
Ben Borges <email@benborges.xyz>
2021-12-02 09:18:13 +0100

    Added 1 tweets

---
+++ data/js/tweets/2021_12.js
@@ -0,0 +1,150 @@
+Grailbird.data.tweets_2021_12 = 
+[
+	{
+		"created_at": "Thu Dec 02 06:57:49 +0000 2021",
+		"id": 1466300555752611800,
+		"id_str": "1466300555752611842",
+		"full_text": "@hridayartha @DerivesSectes @BuddhistLeaks @ringutulku @OgyenKun It's now fully confirmed, OKC sent an email saying it was moved to video conference. It's not organized by them, so from our side, it's good to go and maybe Ringu Tulku will actually listen. But I mean his Belgian agent is one of Spatz/OKC Lawyer, but nobody seems to care.",
+		"truncated": false,
+		"display_text_range": [
+			65,
+			338
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "hridayartha",
+					"name": "hridayartha",
+					"id": 565518712,
+					"id_str": "565518712",
+					"indices": [
+						0,
+						12
+					]
+				},
+				{
+					"screen_name": "DerivesSectes",
+					"name": "Dérives Sectaires / HCG Drifts",
+					"id": 1411708898231345200,
+					"id_str": "1411708898231345161",
+					"indices": [
+						13,
+						27
+					]
+				},
+				{
+					"screen_name": "BuddhistLeaks",
+					"name": "Tibetan Buddhism Leaks",
+					"id": 890549322240196600,
+					"id_str": "890549322240196612",
+					"indices": [
+						28,
+						42
+					]
+				},
+				{
+					"screen_name": "ringutulku",
+					"name": "Ringu Tulku",
+					"id": 137360097,
+					"id_str": "137360097",
+					"indices": [
+						43,
+						54
+					]
+				},
+				{
+					"screen_name": "OgyenKun",
+					"name": "OKC",
+					"id": 3665270417,
+					"id_str": "3665270417",
+					"indices": [
+						55,
+						64
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": 1465666666965217300,
+		"in_reply_to_status_id_str": "1465666666965217289",
+		"in_reply_to_user_id": 565518712,
+		"in_reply_to_user_id_str": "565518712",
+		"in_reply_to_screen_name": "hridayartha",
+		"user": {
+			"id": 3309353195,
+			"id_str": "3309353195",
+			"name": "OKCinfo Research",
+			"screen_name": "OKCinformation",
+			"location": "Belgique",
+			"description": "#OKCinfo is a 2015 Justice initiative from ex-OKC born kids now civil parties to drag Ogyen Kunzang Choling/Robert Spatz @ogyenkun to Justice | contact @rmdes_",
+			"url": "https://t.co/gJaUqhElyi",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/gJaUqhElyi",
+							"expanded_url": "https://okcinfo.news/en",
+							"display_url": "okcinfo.news/en",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 175,
+			"friends_count": 380,
+			"listed_count": 8,
+			"created_at": "Fri Jun 05 15:00:16 +0000 2015",
+			"favourites_count": 328,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1315,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/656530168312262656/Ujovmh2w_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/656530168312262656/Ujovmh2w_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3309353195/1625149299",
+			"profile_link_color": "DD2E44",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@hridayartha @DerivesSectes @BuddhistLeaks @ringutulku @OgyenKun It's now fully confirmed, OKC sent an email saying it was moved to video conference. It's not organized by them, so from our side, it's good to go and maybe Ringu Tulku will actually listen. But I mean his Belgian agent is one of Spatz/OKC Lawyer, but nobody seems to care."
+	}
+]
