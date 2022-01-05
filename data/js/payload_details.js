960e8044bde76028771656bc513dbb38592a29e4
Ben Borges <email@benborges.xyz>
2022-01-05 09:18:12 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 1251,
-	"created_at": "Tue Jan 04 08:18:11 +0000 2022",
+	"tweets": 1252,
+	"created_at": "Wed Jan 05 08:18:11 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_01.js
+++ data/js/tweets/2022_01.js
@@ -1,6 +1,200 @@
 Grailbird.data.tweets_2022_01 = 
 [
 	{
+		"created_at": "Wed Jan 05 06:01:41 +0000 2022",
+		"id": 1478607617656344600,
+		"id_str": "1478607617656344576",
+		"full_text": "\"Systems that cover up abuse through deception, coercion, or abuse of power, mimic the perpetrator and re victimize the victims.\n\nTragically, many lives have been sacrificed on the altar of secrecy for personal gains or the reputation of Tibetan #Buddhism\"\n\nNot @MatthieuRicard https://t.co/twxhB0BKMN",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			277
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "Buddhism",
+					"indices": [
+						246,
+						255
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "MatthieuRicard",
+					"name": "Matthieu Ricard",
+					"id": 181228051,
+					"id_str": "181228051",
+					"indices": [
+						262,
+						277
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1478607614724526000,
+					"id_str": "1478607614724526081",
+					"indices": [
+						278,
+						301
+					],
+					"media_url": "http://pbs.twimg.com/media/FIURyqXX0AE9d2t.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FIURyqXX0AE9d2t.jpg",
+					"url": "https://t.co/twxhB0BKMN",
+					"display_url": "pic.twitter.com/twxhB0BKMN",
+					"expanded_url": "https://twitter.com/OKCinformation/status/1478607617656344576/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 341,
+							"h": 341,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 341,
+							"h": 341,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 341,
+							"h": 341,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1478607614724526000,
+					"id_str": "1478607614724526081",
+					"indices": [
+						278,
+						301
+					],
+					"media_url": "http://pbs.twimg.com/media/FIURyqXX0AE9d2t.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FIURyqXX0AE9d2t.jpg",
+					"url": "https://t.co/twxhB0BKMN",
+					"display_url": "pic.twitter.com/twxhB0BKMN",
+					"expanded_url": "https://twitter.com/OKCinformation/status/1478607617656344576/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 341,
+							"h": 341,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 341,
+							"h": 341,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 341,
+							"h": 341,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3309353195,
+			"id_str": "3309353195",
+			"name": "OKCinfo Research",
+			"screen_name": "OKCinformation",
+			"location": "Belgique",
+			"description": "#OKCinfo is a 2015 Justice initiative from ex-OKC born kids now civil parties to drag Ogyen Kunzang Choling/Robert Spatz @ogyenkun to Justice | contact @rmdes_",
+			"url": "https://t.co/gJaUqhElyi",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/gJaUqhElyi",
+							"expanded_url": "https://okcinfo.news/en",
+							"display_url": "okcinfo.news/en",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 178,
+			"friends_count": 381,
+			"listed_count": 8,
+			"created_at": "Fri Jun 05 15:00:16 +0000 2015",
+			"favourites_count": 330,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1332,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/656530168312262656/Ujovmh2w_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/656530168312262656/Ujovmh2w_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3309353195/1625149299",
+			"profile_link_color": "DD2E44",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "\"Systems that cover up abuse through deception, coercion, or abuse of power, mimic the perpetrator and re victimize the victims.\n\nTragically, many lives have been sacrificed on the altar of secrecy for personal gains or the reputation of Tibetan #Buddhism\"\n\nNot @MatthieuRicard https://t.co/twxhB0BKMN"
+	},
+	{
 		"created_at": "Mon Jan 03 14:20:09 +0000 2022",
 		"id": 1478008281893322800,
 		"id_str": "1478008281893322756",
