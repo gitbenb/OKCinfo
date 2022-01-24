0427202a873773c515de9e43dcf75a321cdb52f9
Ben Borges <email@benborges.xyz>
2022-01-24 09:18:12 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 1255,
-	"created_at": "Fri Jan 21 08:18:11 +0000 2022",
+	"tweets": 1256,
+	"created_at": "Mon Jan 24 08:18:11 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_01.js
+++ data/js/tweets/2022_01.js
@@ -1,6 +1,100 @@
 Grailbird.data.tweets_2022_01 = 
 [
 	{
+		"created_at": "Sun Jan 23 12:40:40 +0000 2022",
+		"id": 1485008826248175600,
+		"id_str": "1485231005363494918",
+		"full_text": "Le 28/01/2022 à 19h13 Ne manquez pas le documentaire de Devoir d'enquête sur le gourou Robert Spatz fondateur d'OKC. \nhttps://t.co/9oL6uaMRwT",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			141
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/9oL6uaMRwT",
+					"expanded_url": "https://www.programme.tv/programme/c228853-devoir-d-enquete/devoir-d-enquete-13854084/",
+					"display_url": "programme.tv/programme/c228…",
+					"indices": [
+						118,
+						141
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1371123515492077600,
+			"id_str": "1371123515492077570",
+			"name": "Les fantômettes traquent les gourous",
+			"screen_name": "GourousLes",
+			"location": "",
+			"description": "",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 331,
+			"friends_count": 60,
+			"listed_count": 3,
+			"created_at": "Sun Mar 14 15:38:27 +0000 2021",
+			"favourites_count": 120,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1589,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1371170582000193539/amcQJL9u_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1371170582000193539/amcQJL9u_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1371123515492077570/1615736395",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 4,
+		"favorite_count": 3,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "fr",
+		"text": "Le 28/01/2022 à 19h13 Ne manquez pas le documentaire de Devoir d'enquête sur le gourou Robert Spatz fondateur d'OKC. \nhttps://t.co/9oL6uaMRwT"
+	},
+	{
 		"created_at": "Thu Jan 20 16:57:16 +0000 2022",
 		"id": 1484199271830274000,
 		"id_str": "1484208417166266375",
