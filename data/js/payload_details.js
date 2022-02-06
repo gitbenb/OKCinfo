da4bad0490f915f26d6954e757e7655e17c9292c
Ben Borges <email@benborges.xyz>
2022-02-06 09:18:13 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 1279,
-	"created_at": "Thu Feb 03 08:18:11 +0000 2022",
+	"tweets": 1280,
+	"created_at": "Sun Feb 06 08:18:11 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_02.js
+++ data/js/tweets/2022_02.js
@@ -1,6 +1,181 @@
 Grailbird.data.tweets_2022_02 = 
 [
 	{
+		"created_at": "Sat Feb 05 08:46:32 +0000 2022",
+		"id": 1489610674087071700,
+		"id_str": "1489883126100811776",
+		"full_text": "OKCinfo est une initiative judiciaire collective visant à demander justice contre l'organisation sectaire Ogyen Kunzang Choling (OKC) et Robert Spatz, pour des crimes d'abus sexuels et physiques contre des enfants/ados, aujourd'hui adultes et parties civiles. https://t.co/lBE6SMRLGH",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			259
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1489610563701325800,
+					"id_str": "1489610563701325826",
+					"indices": [
+						260,
+						283
+					],
+					"media_url": "http://pbs.twimg.com/media/FKwo6lcWQAILKz2.png",
+					"media_url_https": "https://pbs.twimg.com/media/FKwo6lcWQAILKz2.png",
+					"url": "https://t.co/lBE6SMRLGH",
+					"display_url": "pic.twitter.com/lBE6SMRLGH",
+					"expanded_url": "https://twitter.com/OKCinfor/status/1489610674087071750/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 500,
+							"h": 500,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 500,
+							"h": 500,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 500,
+							"h": 500,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1489610563701325800,
+					"id_str": "1489610563701325826",
+					"indices": [
+						260,
+						283
+					],
+					"media_url": "http://pbs.twimg.com/media/FKwo6lcWQAILKz2.png",
+					"media_url_https": "https://pbs.twimg.com/media/FKwo6lcWQAILKz2.png",
+					"url": "https://t.co/lBE6SMRLGH",
+					"display_url": "pic.twitter.com/lBE6SMRLGH",
+					"expanded_url": "https://twitter.com/OKCinfor/status/1489610674087071750/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 500,
+							"h": 500,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 500,
+							"h": 500,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 500,
+							"h": 500,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1097520031955587100,
+			"id_str": "1097520031955587073",
+			"name": "Initiative de Justice OKCinfo",
+			"screen_name": "OKCinfor",
+			"location": "Spatz se pourvoit en cassation",
+			"description": "2021: Instruction en cours France\nBelgique, attente résultat appel cassation \n2020: Condamné 5 ans sursis\nAppel\n2016: Condamné 4 ans sursis\ncontact @rMdes_",
+			"url": "https://t.co/uRVeG10dZv",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/uRVeG10dZv",
+							"expanded_url": "https://okcinfo.news",
+							"display_url": "okcinfo.news",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 108,
+			"friends_count": 322,
+			"listed_count": 0,
+			"created_at": "Mon Feb 18 15:35:41 +0000 2019",
+			"favourites_count": 222,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 822,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1487025431396114438/_3Em3Zfy_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1487025431396114438/_3Em3Zfy_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1097520031955587073/1641541008",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "fr",
+		"text": "OKCinfo est une initiative judiciaire collective visant à demander justice contre l'organisation sectaire Ogyen Kunzang Choling (OKC) et Robert Spatz, pour des crimes d'abus sexuels et physiques contre des enfants/ados, aujourd'hui adultes et parties civiles. https://t.co/lBE6SMRLGH"
+	},
+	{
 		"created_at": "Wed Feb 02 09:33:37 +0000 2022",
 		"id": 1488579831377711000,
 		"id_str": "1488807809160523777",
