3e19f24a1391f4a03a568a59f7f0007a3eb1aa4e
Ben Borges <email@benborges.xyz>
2022-03-24 09:18:13 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 1296,
-	"created_at": "Sat Feb 26 08:18:12 +0000 2022",
+	"tweets": 1297,
+	"created_at": "Thu Mar 24 08:18:12 +0000 2022",
 	"lang": "en"
 }
---
+++ data/js/tweets/2022_03.js
@@ -0,0 +1,186 @@
+Grailbird.data.tweets_2022_03 = 
+[
+	{
+		"created_at": "Wed Mar 23 16:08:20 +0000 2022",
+		"id": 1504770493093761000,
+		"id_str": "1506664151283814403",
+		"full_text": "I see two criminals in this picture, the father and the son. #LamaKunzang alias Robert Spatz.\n\nMeanwhile the Tibetan Buddhist Heads that contributed and enabled the existence of Spatz and Ogyen Kunzang Choling are still silent.\n\nAfter 20 years of inquiry, 2 trials in Belgium. https://t.co/xjBJjMsEUJ",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			276
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "LamaKunzang",
+					"indices": [
+						61,
+						73
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1504770489440510000,
+					"id_str": "1504770489440509956",
+					"indices": [
+						277,
+						300
+					],
+					"media_url": "http://pbs.twimg.com/media/FOIEyZuXoAQsvIm.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FOIEyZuXoAQsvIm.jpg",
+					"url": "https://t.co/xjBJjMsEUJ",
+					"display_url": "pic.twitter.com/xjBJjMsEUJ",
+					"expanded_url": "https://twitter.com/BuddhistLeaks/status/1504770493093761027/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 715,
+							"h": 817,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 715,
+							"h": 817,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 595,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1504770489440510000,
+					"id_str": "1504770489440509956",
+					"indices": [
+						277,
+						300
+					],
+					"media_url": "http://pbs.twimg.com/media/FOIEyZuXoAQsvIm.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FOIEyZuXoAQsvIm.jpg",
+					"url": "https://t.co/xjBJjMsEUJ",
+					"display_url": "pic.twitter.com/xjBJjMsEUJ",
+					"expanded_url": "https://twitter.com/BuddhistLeaks/status/1504770493093761027/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 715,
+							"h": 817,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 715,
+							"h": 817,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 595,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 890549322240196600,
+			"id_str": "890549322240196612",
+			"name": "Tibetan Buddhism Leaks",
+			"screen_name": "BuddhistLeaks",
+			"location": "Dharamsala",
+			"description": "Amplifying comments & news regarding Tibetan Buddhism sexual abuses allegations crimes : ⛔ FPMT, Shambhala, RIGPA, FWBO, NKT, Triratna, Ogyen Kunzang Choling",
+			"url": "https://t.co/hUPUjKwEpU",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/hUPUjKwEpU",
+							"expanded_url": "https://AbusesInTibetanBuddhism.info",
+							"display_url": "AbusesInTibetanBuddhism.info",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 584,
+			"friends_count": 1677,
+			"listed_count": 0,
+			"created_at": "Thu Jul 27 12:27:58 +0000 2017",
+			"favourites_count": 337,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 12531,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1340250942374105089/kAd0Uc3w_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1340250942374105089/kAd0Uc3w_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/890549322240196612/1608375674",
+			"profile_link_color": "FF691F",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "I see two criminals in this picture, the father and the son. #LamaKunzang alias Robert Spatz.\n\nMeanwhile the Tibetan Buddhist Heads that contributed and enabled the existence of Spatz and Ogyen Kunzang Choling are still silent.\n\nAfter 20 years of inquiry, 2 trials in Belgium. https://t.co/xjBJjMsEUJ"
+	}
+]
