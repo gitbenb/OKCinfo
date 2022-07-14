586284eb5fc8e609e32cbcc42852fd798d8570ee
Ben Borges <email@benborges.xyz>
2022-07-14 10:18:12 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 1309,
-	"created_at": "Tue Jun 21 08:18:11 +0000 2022",
+	"tweets": 1310,
+	"created_at": "Thu Jul 14 08:18:11 +0000 2022",
 	"lang": "en"
 }
---
+++ data/js/tweets/2022_07.js
@@ -0,0 +1,284 @@
+Grailbird.data.tweets_2022_07 = 
+[
+	{
+		"created_at": "Wed Jul 13 09:02:22 +0000 2022",
+		"id": 1547141062468198400,
+		"id_str": "1547144394721181699",
+		"full_text": "Documentaire ARTE bientôt sur vos écrans. À la rentrée 🤞 https://t.co/PZINTaljwe",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			56
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/PZINTaljwe",
+					"expanded_url": "https://twitter.com/ntrouiller/status/1546480157858402305",
+					"display_url": "twitter.com/ntrouiller/sta…",
+					"indices": [
+						57,
+						80
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1097520031955587100,
+			"id_str": "1097520031955587073",
+			"name": "Initiative de Justice OKCinfo",
+			"screen_name": "OKCinfor",
+			"location": " France-Belgique",
+			"description": "2021: Instruction en cours France\nBelgique, attente résultat appel cassation \n2020: Condamné 5 ans sursis\nAppel\n2016: Condamné 4 ans sursis\ncontact @rMdes_",
+			"url": "https://t.co/uRVeG10dZv",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/uRVeG10dZv",
+							"expanded_url": "https://okcinfo.news",
+							"display_url": "okcinfo.news",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 123,
+			"friends_count": 323,
+			"listed_count": 0,
+			"created_at": "Mon Feb 18 15:35:41 +0000 2019",
+			"favourites_count": 244,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 857,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1487025431396114438/_3Em3Zfy_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1487025431396114438/_3Em3Zfy_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1097520031955587073/1641541008",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1546480157858402300,
+		"quoted_status_id_str": "1546480157858402305",
+		"quoted_status_permalink": {
+			"url": "https://t.co/PZINTaljwe",
+			"expanded": "https://twitter.com/ntrouiller/status/1546480157858402305",
+			"display": "twitter.com/ntrouiller/sta…"
+		},
+		"quoted_status": {
+			"created_at": "Mon Jul 11 13:02:56 +0000 2022",
+			"id": 1546480157858402300,
+			"id_str": "1546480157858402305",
+			"full_text": "- Sur l'horrible affaire OKC que je ne maîtrise pas suffisamment: @DerivesSectes, @OKCinfor et le fabuleux travail de @kalou ici, d'autant qu'un procès va arriver en France :https://t.co/cla0lTj1Bl\n\n- Sur les dossiers du bouddhisme: @BuddhistLeaks, @openbuddhism1 @robhogendoorn",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				278
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "DerivesSectes",
+						"name": "Dérives Sectaires / HCG Drifts",
+						"id": 1411708898231345200,
+						"id_str": "1411708898231345161",
+						"indices": [
+							66,
+							80
+						]
+					},
+					{
+						"screen_name": "OKCinfor",
+						"name": "Initiative de Justice OKCinfo",
+						"id": 1097520031955587100,
+						"id_str": "1097520031955587073",
+						"indices": [
+							82,
+							91
+						]
+					},
+					{
+						"screen_name": "kalou",
+						"name": "kalou • #CitoyensDuWeb",
+						"id": 11834812,
+						"id_str": "11834812",
+						"indices": [
+							118,
+							124
+						]
+					},
+					{
+						"screen_name": "BuddhistLeaks",
+						"name": "Tibetan Buddhism Leaks",
+						"id": 890549322240196600,
+						"id_str": "890549322240196612",
+						"indices": [
+							233,
+							247
+						]
+					},
+					{
+						"screen_name": "openbuddhism1",
+						"name": "openbuddhism",
+						"id": 1140961485352525800,
+						"id_str": "1140961485352525825",
+						"indices": [
+							249,
+							263
+						]
+					},
+					{
+						"screen_name": "robhogendoorn",
+						"name": "robhogendoorn",
+						"id": 1396358893949931500,
+						"id_str": "1396358893949931521",
+						"indices": [
+							264,
+							278
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/cla0lTj1Bl",
+						"expanded_url": "https://www.youtube.com/watch?v=pftPi3IDlXc",
+						"display_url": "youtube.com/watch?v=pftPi3…",
+						"indices": [
+							174,
+							197
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+			"in_reply_to_status_id": 1546480155304272000,
+			"in_reply_to_status_id_str": "1546480155304271873",
+			"in_reply_to_user_id": 116193827,
+			"in_reply_to_user_id_str": "116193827",
+			"in_reply_to_screen_name": "ntrouiller",
+			"user": {
+				"id": 116193827,
+				"id_str": "116193827",
+				"name": "Natalia Trouiller",
+				"screen_name": "ntrouiller",
+				"location": "Lyon",
+				"description": "Écrivain, conférencière. Réflexion sur la tech, patristique, gnosis hunter. Aime Jésus et son Église. Car même quand ma mère dévisse gravement, ça reste ma mère",
+				"url": "https://t.co/nsKqRURwwg",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/nsKqRURwwg",
+								"expanded_url": "https://www.debunkinprogress.org",
+								"display_url": "debunkinprogress.org",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 6805,
+				"friends_count": 1328,
+				"listed_count": 142,
+				"created_at": "Sun Feb 21 14:33:01 +0000 2010",
+				"favourites_count": 10106,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 17483,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1437129498080817153/D5HYZPIJ_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1437129498080817153/D5HYZPIJ_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/116193827/1634540168",
+				"profile_link_color": "0084B4",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": false,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 7,
+			"favorite_count": 39,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "fr"
+		},
+		"retweet_count": 2,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "fr",
+		"text": "Documentaire ARTE bientôt sur vos écrans. À la rentrée 🤞 https://t.co/PZINTaljwe"
+	}
+]
