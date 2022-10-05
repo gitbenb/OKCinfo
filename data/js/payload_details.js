57175f1e62b5a8e038f0a85eb23895fc6b1d2a83
Ben Borges <email@benborges.xyz>
2022-10-05 10:18:13 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 1525,
-	"created_at": "Mon Oct 03 08:18:13 +0000 2022",
+	"tweets": 1526,
+	"created_at": "Wed Oct 05 08:18:12 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_10.js
+++ data/js/tweets/2022_10.js
@@ -1,6 +1,181 @@
 Grailbird.data.tweets_2022_10 = 
 [
 	{
+		"created_at": "Wed Oct 05 06:15:10 +0000 2022",
+		"id": 1577542837067030500,
+		"id_str": "1577542894998650883",
+		"full_text": "C'est aujourd'hui. 2 ans qu'on attendait.\n\nOn espère un rejet du pourvoi en Cassation de l'accusé principal, absent depuis 20 ans des procédures judiciaires.\n\nOn l'attend en France au tournant parce qu'il a fallu tout recommencer proprement.\n\nAprès 20 ans d'instructions 🇧🇪 https://t.co/dsyDKrlaDA",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			273
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1577542834848206800,
+					"id_str": "1577542834848206848",
+					"indices": [
+						274,
+						297
+					],
+					"media_url": "http://pbs.twimg.com/media/FeSO2aEXEAAyKhk.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FeSO2aEXEAAyKhk.jpg",
+					"url": "https://t.co/dsyDKrlaDA",
+					"display_url": "pic.twitter.com/dsyDKrlaDA",
+					"expanded_url": "https://twitter.com/OKCinfor/status/1577542837067030528/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 599,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 970,
+							"h": 855,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 970,
+							"h": 855,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1577542834848206800,
+					"id_str": "1577542834848206848",
+					"indices": [
+						274,
+						297
+					],
+					"media_url": "http://pbs.twimg.com/media/FeSO2aEXEAAyKhk.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FeSO2aEXEAAyKhk.jpg",
+					"url": "https://t.co/dsyDKrlaDA",
+					"display_url": "pic.twitter.com/dsyDKrlaDA",
+					"expanded_url": "https://twitter.com/OKCinfor/status/1577542837067030528/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 599,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 970,
+							"h": 855,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 970,
+							"h": 855,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1097520031955587100,
+			"id_str": "1097520031955587073",
+			"name": "Initiative de Justice OKCinfo",
+			"screen_name": "OKCinfor",
+			"location": " 🇫🇷🇧🇪🇪🇸🇵🇹",
+			"description": "2022: 🇫🇷Instruction en cours \n🇧🇪 verdict cassation 5 octobre 2022 | \n2020: Condamné 5 ans sursis | \n2016: Condamné 4 ans sursis\ncontact @rMdes_",
+			"url": "https://t.co/uRVeG10dZv",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/uRVeG10dZv",
+							"expanded_url": "https://okcinfo.news",
+							"display_url": "okcinfo.news",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 225,
+			"friends_count": 354,
+			"listed_count": 2,
+			"created_at": "Mon Feb 18 15:35:41 +0000 2019",
+			"favourites_count": 375,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1230,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1487025431396114438/_3Em3Zfy_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1487025431396114438/_3Em3Zfy_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1097520031955587073/1641541008",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 4,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "fr",
+		"text": "C'est aujourd'hui. 2 ans qu'on attendait.\n\nOn espère un rejet du pourvoi en Cassation de l'accusé principal, absent depuis 20 ans des procédures judiciaires.\n\nOn l'attend en France au tournant parce qu'il a fallu tout recommencer proprement.\n\nAprès 20 ans d'instructions 🇧🇪 https://t.co/dsyDKrlaDA"
+	},
+	{
 		"created_at": "Sun Oct 02 12:08:44 +0000 2022",
 		"id": 1576531936402870300,
 		"id_str": "1576544710679617537",
