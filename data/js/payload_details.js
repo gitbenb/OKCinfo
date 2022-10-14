9f45411c3bd9f761f7338b3b2245cec00eb197ad
Ben Borges <email@benborges.xyz>
2022-10-14 10:18:14 +0200

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 1564,
-	"created_at": "Wed Oct 12 08:18:13 +0000 2022",
+	"tweets": 1566,
+	"created_at": "Fri Oct 14 08:18:13 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_10.js
+++ data/js/tweets/2022_10.js
@@ -1,6 +1,270 @@
 Grailbird.data.tweets_2022_10 = 
 [
 	{
+		"created_at": "Fri Oct 14 05:42:44 +0000 2022",
+		"id": 1580796223334023200,
+		"id_str": "1580796223334023168",
+		"full_text": "@EygesJeffrey @beyourownguide_ @robhogendoorn Try out @mullvadnet\nIt's easy to install and it protect your privacy online, allows you to switch location around the world. Pick any European city such as Berlin or Paris to watch it.",
+		"truncated": false,
+		"display_text_range": [
+			46,
+			230
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "EygesJeffrey",
+					"name": "Jeffrey Eyges💉🇺🇦 #ExpandTheCourt #BLM #M4A",
+					"id": 1328844194975260700,
+					"id_str": "1328844194975260676",
+					"indices": [
+						0,
+						13
+					]
+				},
+				{
+					"screen_name": "beyourownguide_",
+					"name": "a",
+					"id": 927267702422495200,
+					"id_str": "927267702422495232",
+					"indices": [
+						14,
+						30
+					]
+				},
+				{
+					"screen_name": "robhogendoorn",
+					"name": "robhogendoorn",
+					"id": 1396358893949931500,
+					"id_str": "1396358893949931521",
+					"indices": [
+						31,
+						45
+					]
+				},
+				{
+					"screen_name": "mullvadnet",
+					"name": "Mullvad.net",
+					"id": 38818843,
+					"id_str": "38818843",
+					"indices": [
+						54,
+						65
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": 1579451765363445800,
+		"in_reply_to_status_id_str": "1579451765363445760",
+		"in_reply_to_user_id": 1328844194975260700,
+		"in_reply_to_user_id_str": "1328844194975260676",
+		"in_reply_to_screen_name": "EygesJeffrey",
+		"user": {
+			"id": 3309353195,
+			"id_str": "3309353195",
+			"name": "OKCinfo Research",
+			"screen_name": "OKCinformation",
+			"location": "Belgique",
+			"description": "#OKCinfo is an ongoing Justice initiative from ex-OKC born kids now civil parties to drag Ogyen Kunzang Choling/Robert Spatz Lama Kunzang to justice & jail it!",
+			"url": "https://t.co/zYcyQtvumv",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/zYcyQtvumv",
+							"expanded_url": "https://okcinfo.news/en",
+							"display_url": "okcinfo.news/en",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 210,
+			"friends_count": 386,
+			"listed_count": 7,
+			"created_at": "Fri Jun 05 15:00:16 +0000 2015",
+			"favourites_count": 353,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1671,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/656530168312262656/Ujovmh2w_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/656530168312262656/Ujovmh2w_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3309353195/1625149299",
+			"profile_link_color": "DD2E44",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@EygesJeffrey @beyourownguide_ @robhogendoorn Try out @mullvadnet\nIt's easy to install and it protect your privacy online, allows you to switch location around the world. Pick any European city such as Berlin or Paris to watch it."
+	},
+	{
+		"created_at": "Fri Oct 14 05:42:28 +0000 2022",
+		"id": 1580796158699393000,
+		"id_str": "1580796158699393025",
+		"full_text": "@beyourownguide_ @robhogendoorn Try out @mullvadnet\nIt's easy to install and it protect your privacy online, allows you to switch location around the world. Pick any European city such as Berlin or Paris to watch it.",
+		"truncated": false,
+		"display_text_range": [
+			32,
+			216
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "beyourownguide_",
+					"name": "a",
+					"id": 927267702422495200,
+					"id_str": "927267702422495232",
+					"indices": [
+						0,
+						16
+					]
+				},
+				{
+					"screen_name": "robhogendoorn",
+					"name": "robhogendoorn",
+					"id": 1396358893949931500,
+					"id_str": "1396358893949931521",
+					"indices": [
+						17,
+						31
+					]
+				},
+				{
+					"screen_name": "mullvadnet",
+					"name": "Mullvad.net",
+					"id": 38818843,
+					"id_str": "38818843",
+					"indices": [
+						40,
+						51
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": 1579399112616992800,
+		"in_reply_to_status_id_str": "1579399112616992768",
+		"in_reply_to_user_id": 927267702422495200,
+		"in_reply_to_user_id_str": "927267702422495232",
+		"in_reply_to_screen_name": "beyourownguide_",
+		"user": {
+			"id": 3309353195,
+			"id_str": "3309353195",
+			"name": "OKCinfo Research",
+			"screen_name": "OKCinformation",
+			"location": "Belgique",
+			"description": "#OKCinfo is an ongoing Justice initiative from ex-OKC born kids now civil parties to drag Ogyen Kunzang Choling/Robert Spatz Lama Kunzang to justice & jail it!",
+			"url": "https://t.co/zYcyQtvumv",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/zYcyQtvumv",
+							"expanded_url": "https://okcinfo.news/en",
+							"display_url": "okcinfo.news/en",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 210,
+			"friends_count": 386,
+			"listed_count": 7,
+			"created_at": "Fri Jun 05 15:00:16 +0000 2015",
+			"favourites_count": 353,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1671,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/656530168312262656/Ujovmh2w_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/656530168312262656/Ujovmh2w_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3309353195/1625149299",
+			"profile_link_color": "DD2E44",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@beyourownguide_ @robhogendoorn Try out @mullvadnet\nIt's easy to install and it protect your privacy online, allows you to switch location around the world. Pick any European city such as Berlin or Paris to watch it."
+	},
+	{
 		"created_at": "Wed Oct 12 06:34:36 +0000 2022",
 		"id": 1579409713644859400,
 		"id_str": "1580084500880818177",
