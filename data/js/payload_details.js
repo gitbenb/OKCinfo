a79f19e0116fbacfe33036642592fe00ccb56300
Ben Borges <email@benborges.xyz>
2022-11-04 09:18:13 +0100

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 1568,
-	"created_at": "Mon Oct 31 08:18:13 +0000 2022",
+	"tweets": 1570,
+	"created_at": "Fri Nov 04 08:18:13 +0000 2022",
 	"lang": "en"
 }
---
+++ data/js/tweets/2022_11.js
@@ -0,0 +1,528 @@
+Grailbird.data.tweets_2022_11 = 
+[
+	{
+		"created_at": "Thu Nov 03 12:39:49 +0000 2022",
+		"id": 1588148852552523800,
+		"id_str": "1588148946567938048",
+		"full_text": "Dommage que la seule mention de Bouddhisme Tibétain dans le rapport 2021 @Miviludes_Gouv soit limitée à Chogyam Trungpa, sans même mentionner +30 ans de dérives et d'abus #Shambhala, pourtant largement connu. Ou #RIGPA ou encore Ogyen Kunzang Choling (instructions🇫🇷 en cours) https://t.co/HHZTSbUw08",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			276
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "Shambhala",
+					"indices": [
+						171,
+						181
+					]
+				},
+				{
+					"text": "RIGPA",
+					"indices": [
+						212,
+						218
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Miviludes_Gouv",
+					"name": "Miviludes",
+					"id": 1389966435292700700,
+					"id_str": "1389966435292700677",
+					"indices": [
+						73,
+						88
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1588148844973506600,
+					"id_str": "1588148844973506560",
+					"indices": [
+						277,
+						300
+					],
+					"media_url": "http://pbs.twimg.com/media/Fgo89csXwAAiIlC.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fgo89csXwAAiIlC.jpg",
+					"url": "https://t.co/HHZTSbUw08",
+					"display_url": "pic.twitter.com/HHZTSbUw08",
+					"expanded_url": "https://twitter.com/OKCinfor/status/1588148852552523777/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1072,
+							"h": 1803,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 404,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 713,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1588148844973506600,
+					"id_str": "1588148844973506560",
+					"indices": [
+						277,
+						300
+					],
+					"media_url": "http://pbs.twimg.com/media/Fgo89csXwAAiIlC.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Fgo89csXwAAiIlC.jpg",
+					"url": "https://t.co/HHZTSbUw08",
+					"display_url": "pic.twitter.com/HHZTSbUw08",
+					"expanded_url": "https://twitter.com/OKCinfor/status/1588148852552523777/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1072,
+							"h": 1803,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 404,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 713,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1097520031955587100,
+			"id_str": "1097520031955587073",
+			"name": "Initiative de Justice OKCinfo",
+			"screen_name": "OKCinfor",
+			"location": " 🇫🇷🇧🇪🇪🇸🇵🇹",
+			"description": "2022: 🇫🇷Instruction en cours\n🇧🇪 Rejet du pourvoi en cassation clap de fin | \n2020: Condamné 5 ans sursis | \n2016: Condamné 4 ans sursis\ncontact @rMdes_",
+			"url": "https://t.co/uRVeG1hOR3",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/uRVeG1hOR3",
+							"expanded_url": "https://okcinfo.news",
+							"display_url": "okcinfo.news",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 231,
+			"friends_count": 368,
+			"listed_count": 1,
+			"created_at": "Mon Feb 18 15:35:41 +0000 2019",
+			"favourites_count": 386,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1278,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1487025431396114438/_3Em3Zfy_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1487025431396114438/_3Em3Zfy_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1097520031955587073/1667149629",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 8,
+		"favorite_count": 12,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "fr",
+		"text": "Dommage que la seule mention de Bouddhisme Tibétain dans le rapport 2021 @Miviludes_Gouv soit limitée à Chogyam Trungpa, sans même mentionner +30 ans de dérives et d'abus #Shambhala, pourtant largement connu. Ou #RIGPA ou encore Ogyen Kunzang Choling (instructions🇫🇷 en cours) https://t.co/HHZTSbUw08"
+	},
+	{
+		"created_at": "Thu Nov 03 11:51:21 +0000 2022",
+		"id": 1588102508546871300,
+		"id_str": "1588136746633646080",
+		"full_text": "C'est pas de la spéculation, c'est pas une blague.. Après avoir retracé 40 ans d'abus sexuels en toute impunité, le modus operandi devient clair et les suspicions de 2010 (2 cas) se sont avérées prouvées en 2020.\n\nC'est quand qu'on va prendre cette histoire au sérieux ? https://t.co/LkHQmC2gjf",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			270
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/LkHQmC2gjf",
+					"expanded_url": "https://twitter.com/OKCinfor/status/1587847778285273096",
+					"display_url": "twitter.com/OKCinfor/statu…",
+					"indices": [
+						271,
+						294
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1562961949,
+			"id_str": "1562961949",
+			"name": "Ricardo Mendes",
+			"screen_name": "rMdes_",
+			"location": "🇪🇺🇵🇹🇺🇦🇧🇪😷💉💉💉💉",
+			"description": "Passionné par l'information, la démocratie, la justice et le discernement | @PolBegov @EUwatchers Porte-parole/Spokesperson @OKCinfor",
+			"url": "https://t.co/zNucbspDfn",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/zNucbspDfn",
+							"expanded_url": "https://rmendes.net/",
+							"display_url": "rmendes.net",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2588,
+			"friends_count": 4996,
+			"listed_count": 326,
+			"created_at": "Tue Jul 02 11:30:45 +0000 2013",
+			"favourites_count": 14830,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 43999,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1576114396400590848/7zquaAE__normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1576114396400590848/7zquaAE__normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1562961949/1659387530",
+			"profile_link_color": "3B94D9",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1587847778285273000,
+		"quoted_status_id_str": "1587847778285273096",
+		"quoted_status_permalink": {
+			"url": "https://t.co/LkHQmC2gjf",
+			"expanded": "https://twitter.com/OKCinfor/status/1587847778285273096",
+			"display": "twitter.com/OKCinfor/statu…"
+		},
+		"quoted_status": {
+			"created_at": "Wed Nov 02 16:43:05 +0000 2022",
+			"id": 1587847778285273000,
+			"id_str": "1587847778285273096",
+			"full_text": "Pedocriminal convicted twice in #Belgium, residing in #Malaga, at least two cases of sexual abuses ongoing. \n#Spain #España #MeToo https://t.co/wHSG9gUP9U",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				130
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "Belgium",
+						"indices": [
+							32,
+							40
+						]
+					},
+					{
+						"text": "Malaga",
+						"indices": [
+							54,
+							61
+						]
+					},
+					{
+						"text": "Spain",
+						"indices": [
+							109,
+							115
+						]
+					},
+					{
+						"text": "España",
+						"indices": [
+							116,
+							123
+						]
+					},
+					{
+						"text": "MeToo",
+						"indices": [
+							124,
+							130
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1587846745446293500,
+						"id_str": "1587846745446293510",
+						"indices": [
+							131,
+							154
+						],
+						"media_url": "http://pbs.twimg.com/media/FgkqM8QWIAYe0Ij.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/FgkqM8QWIAYe0Ij.jpg",
+						"url": "https://t.co/wHSG9gUP9U",
+						"display_url": "pic.twitter.com/wHSG9gUP9U",
+						"expanded_url": "https://twitter.com/OKCinfor/status/1587847778285273096/photo/1",
+						"type": "photo",
+						"sizes": {
+							"small": {
+								"w": 680,
+								"h": 369,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 652,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1416,
+								"h": 769,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1587846745446293500,
+						"id_str": "1587846745446293510",
+						"indices": [
+							131,
+							154
+						],
+						"media_url": "http://pbs.twimg.com/media/FgkqM8QWIAYe0Ij.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/FgkqM8QWIAYe0Ij.jpg",
+						"url": "https://t.co/wHSG9gUP9U",
+						"display_url": "pic.twitter.com/wHSG9gUP9U",
+						"expanded_url": "https://twitter.com/OKCinfor/status/1587847778285273096/photo/1",
+						"type": "photo",
+						"sizes": {
+							"small": {
+								"w": 680,
+								"h": 369,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 652,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1416,
+								"h": 769,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 1097520031955587100,
+				"id_str": "1097520031955587073",
+				"name": "Initiative de Justice OKCinfo",
+				"screen_name": "OKCinfor",
+				"location": " 🇫🇷🇧🇪🇪🇸🇵🇹",
+				"description": "2022: 🇫🇷Instruction en cours\n🇧🇪 Rejet du pourvoi en cassation clap de fin | \n2020: Condamné 5 ans sursis | \n2016: Condamné 4 ans sursis\ncontact @rMdes_",
+				"url": "https://t.co/uRVeG1hOR3",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/uRVeG1hOR3",
+								"expanded_url": "https://okcinfo.news",
+								"display_url": "okcinfo.news",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 231,
+				"friends_count": 368,
+				"listed_count": 1,
+				"created_at": "Mon Feb 18 15:35:41 +0000 2019",
+				"favourites_count": 386,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 1278,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "F5F8FA",
+				"profile_background_image_url": null,
+				"profile_background_image_url_https": null,
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1487025431396114438/_3Em3Zfy_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1487025431396114438/_3Em3Zfy_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/1097520031955587073/1667149629",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": true,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 4,
+			"favorite_count": 1,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 4,
+		"favorite_count": 3,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "fr",
+		"text": "C'est pas de la spéculation, c'est pas une blague.. Après avoir retracé 40 ans d'abus sexuels en toute impunité, le modus operandi devient clair et les suspicions de 2010 (2 cas) se sont avérées prouvées en 2020.\n\nC'est quand qu'on va prendre cette histoire au sérieux ? https://t.co/LkHQmC2gjf"
+	}
+]
