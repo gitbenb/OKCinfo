19cc4cae072fdac4eaee4ab8212b493d477a063c
Ben Borges <email@benborges.xyz>
2022-11-05 09:18:13 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 1570,
-	"created_at": "Fri Nov 04 08:18:13 +0000 2022",
+	"tweets": 1571,
+	"created_at": "Sat Nov 05 08:18:12 +0000 2022",
 	"lang": "en"
 }
--- data/js/tweets/2022_11.js
+++ data/js/tweets/2022_11.js
@@ -1,6 +1,143 @@
 Grailbird.data.tweets_2022_11 = 
 [
 	{
+		"created_at": "Fri Nov 04 10:31:32 +0000 2022",
+		"id": 1588469689180733400,
+		"id_str": "1588479048959021056",
+		"full_text": "@DerivesSectes @OKCinfor @Miviludes_Gouv \"On réalise que la mission de lutter contre le phénomène sectaire en France est assurée par 9 personnes.\"\nhttps://t.co/cHlLnRRV0q",
+		"truncated": false,
+		"display_text_range": [
+			41,
+			170
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "DerivesSectes",
+					"name": "DS/SD",
+					"id": 1411708898231345200,
+					"id_str": "1411708898231345161",
+					"indices": [
+						0,
+						14
+					]
+				},
+				{
+					"screen_name": "OKCinfor",
+					"name": "Initiative de Justice OKCinfo",
+					"id": 1097520031955587100,
+					"id_str": "1097520031955587073",
+					"indices": [
+						15,
+						24
+					]
+				},
+				{
+					"screen_name": "Miviludes_Gouv",
+					"name": "Miviludes",
+					"id": 1389966435292700700,
+					"id_str": "1389966435292700677",
+					"indices": [
+						25,
+						40
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/cHlLnRRV0q",
+					"expanded_url": "https://bloglextracteur.wordpress.com/2022/11/04/ce-qui-ne-vous-a-pas-ete-dit-sur-le-dernier-rapport-de-la-miviludes/",
+					"display_url": "bloglextracteur.wordpress.com/2022/11/04/ce-…",
+					"indices": [
+						147,
+						170
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1588456245685457000,
+		"in_reply_to_status_id_str": "1588456245685456896",
+		"in_reply_to_user_id": 565518712,
+		"in_reply_to_user_id_str": "565518712",
+		"in_reply_to_screen_name": "hridayartha",
+		"user": {
+			"id": 565518712,
+			"id_str": "565518712",
+			"name": "hridayartha",
+			"screen_name": "hridayartha",
+			"location": "France",
+			"description": "Compte Twitter associé au blog Dans le Sillage d'Advayavajra",
+			"url": "http://t.co/fLn1I35iNa",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/fLn1I35iNa",
+							"expanded_url": "http://hridayartha.blogspot.fr/",
+							"display_url": "hridayartha.blogspot.fr",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 126,
+			"friends_count": 382,
+			"listed_count": 6,
+			"created_at": "Sat Apr 28 15:17:32 +0000 2012",
+			"favourites_count": 3115,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 4140,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1191009879793778694/0prPxIsk_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1191009879793778694/0prPxIsk_normal.jpg",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 4,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "fr",
+		"text": "@DerivesSectes @OKCinfor @Miviludes_Gouv \"On réalise que la mission de lutter contre le phénomène sectaire en France est assurée par 9 personnes.\"\nhttps://t.co/cHlLnRRV0q"
+	},
+	{
 		"created_at": "Thu Nov 03 12:39:49 +0000 2022",
 		"id": 1588148852552523800,
 		"id_str": "1588148946567938048",
