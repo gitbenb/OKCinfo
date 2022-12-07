041eb5ff697eafc4d5f3183581b3c390953ae25f
Ben Borges <email@benborges.xyz>
2022-12-07 09:18:13 +0100

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 1579,
-	"created_at": "Sat Nov 26 08:18:12 +0000 2022",
+	"tweets": 1581,
+	"created_at": "Wed Dec 07 08:18:12 +0000 2022",
 	"lang": "en"
 }
---
+++ data/js/tweets/2022_12.js
@@ -0,0 +1,274 @@
+Grailbird.data.tweets_2022_12 = 
+[
+	{
+		"created_at": "Wed Dec 07 05:36:47 +0000 2022",
+		"id": 1600191654840848400,
+		"id_str": "1600363670566883328",
+		"full_text": "what are the \"karmic consequences\" of enabling, allowing, sexual abusers &amp; Predators in the midst of several Tibetan Schools Lineages that were supposed to be Authentic and Uninterrupted.\n\nBlind to their own drift while pretending to be Lineage  holders. https://t.co/X3tL2Xd3tI",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			258
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1600191637770076200,
+					"id_str": "1600191637770076170",
+					"indices": [
+						259,
+						282
+					],
+					"media_url": "http://pbs.twimg.com/media/FjUF0P3WAAowxBs.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FjUF0P3WAAowxBs.jpg",
+					"url": "https://t.co/X3tL2Xd3tI",
+					"display_url": "pic.twitter.com/X3tL2Xd3tI",
+					"expanded_url": "https://twitter.com/BuddhistLeaks/status/1600191654840848384/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1080,
+							"h": 360,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 227,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 360,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1600191637770076200,
+					"id_str": "1600191637770076170",
+					"indices": [
+						259,
+						282
+					],
+					"media_url": "http://pbs.twimg.com/media/FjUF0P3WAAowxBs.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FjUF0P3WAAowxBs.jpg",
+					"url": "https://t.co/X3tL2Xd3tI",
+					"display_url": "pic.twitter.com/X3tL2Xd3tI",
+					"expanded_url": "https://twitter.com/BuddhistLeaks/status/1600191654840848384/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1080,
+							"h": 360,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 227,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 360,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 890549322240196600,
+			"id_str": "890549322240196612",
+			"name": "Tibetan Buddhism Leaks",
+			"screen_name": "BuddhistLeaks",
+			"location": "Dharamsala",
+			"description": "Amplifying comments & news regarding Tibetan Buddhism sexual abuses crimes : ⛔ FPMT, Shambhala, RIGPA, FWBO, NKT, Triratna, Ogyen Kunzang Choling/OKC",
+			"url": "https://t.co/hUPUjKxcfs",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/hUPUjKxcfs",
+							"expanded_url": "https://AbusesInTibetanBuddhism.info",
+							"display_url": "AbusesInTibetanBuddhism.info",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 728,
+			"friends_count": 1719,
+			"listed_count": 0,
+			"created_at": "Thu Jul 27 12:27:58 +0000 2017",
+			"favourites_count": 374,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 13243,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1340250942374105089/kAd0Uc3w_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1340250942374105089/kAd0Uc3w_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/890549322240196612/1608375674",
+			"profile_link_color": "FF691F",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 7,
+		"favorite_count": 7,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "what are the \"karmic consequences\" of enabling, allowing, sexual abusers &amp; Predators in the midst of several Tibetan Schools Lineages that were supposed to be Authentic and Uninterrupted.\n\nBlind to their own drift while pretending to be Lineage  holders. https://t.co/X3tL2Xd3tI"
+	},
+	{
+		"created_at": "Tue Dec 06 11:20:17 +0000 2022",
+		"id": 1599841679837565000,
+		"id_str": "1600087729110274048",
+		"full_text": "Let me be frank here. This is the most uninformed op-ed I’ve read in years on access to justice for sexual assault victims. These were the tropes embraced by abusers and the entities that able them. The victim didn’t come forward soon enough. Right!",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			249
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 355565919,
+			"id_str": "355565919",
+			"name": "Marci A. Hamilton",
+			"screen_name": "Marci_Hamilton",
+			"location": "New York, NY",
+			"description": "Professor, University of Pennsylvania. Founder & CEO, @CHILD_USA. Expert on SOL reform. Author of Justice Denied and God vs. the Gavel. She/Her.",
+			"url": "https://t.co/cOhDXsZT10",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/cOhDXsZT10",
+							"expanded_url": "http://www.childusa.org",
+							"display_url": "childusa.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4300,
+			"friends_count": 791,
+			"listed_count": 85,
+			"created_at": "Mon Aug 15 15:15:47 +0000 2011",
+			"favourites_count": 915,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 6590,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1517156818799243265/bH0R3S0F_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1517156818799243265/bH0R3S0F_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/355565919/1396729968",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 13,
+		"favorite_count": 37,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "en",
+		"text": "Let me be frank here. This is the most uninformed op-ed I’ve read in years on access to justice for sexual assault victims. These were the tropes embraced by abusers and the entities that able them. The victim didn’t come forward soon enough. Right!"
+	}
+]
