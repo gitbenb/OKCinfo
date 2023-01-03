2bb88ab4958ae471d68d0d428bbcbed7e7b8944e
Ben Borges <email@benborges.xyz>
2023-01-03 09:18:13 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 1593,
-	"created_at": "Tue Dec 27 08:18:12 +0000 2022",
+	"tweets": 1594,
+	"created_at": "Tue Jan 03 08:18:12 +0000 2023",
 	"lang": "en"
 }
---
+++ data/js/tweets/2023_01.js
@@ -0,0 +1,118 @@
+Grailbird.data.tweets_2023_01 = 
+[
+	{
+		"created_at": "Mon Jan 02 10:16:24 +0000 2023",
+		"id": 1609093387054944300,
+		"id_str": "1609856125939195905",
+		"full_text": "Space #okcinfo vendredi prochain !\n\nOn va brièvement aborder la fin de l'affaire belge OKC-Spatz, rejet de l'appel des accusés \n\nLes suites de l'instruction en France\n\nLa lente prise de conscience du Bouddhisme Tibétain sur sa propre responsabilité. \n\nhttps://t.co/6SB1qd6MqP",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			275
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "okcinfo",
+					"indices": [
+						6,
+						14
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/6SB1qd6MqP",
+					"expanded_url": "https://twitter.com/i/spaces/1zqJVPppvZZKB",
+					"display_url": "twitter.com/i/spaces/1zqJV…",
+					"indices": [
+						252,
+						275
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1562961949,
+			"id_str": "1562961949",
+			"name": "Ricardo Mendes",
+			"screen_name": "rMdes_",
+			"location": "🇪🇺🇵🇹🇺🇦🇧🇪😷💉💉💉💉",
+			"description": "Passionné par l'information, la démocratie, la justice et le discernement | Porte-parole/Partie Civile @OKCinfor ¦   \n🐘@rmdes@mstdn.social",
+			"url": "https://t.co/zNucbsHeDX",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/zNucbsHeDX",
+							"expanded_url": "https://rmendes.net/",
+							"display_url": "rmendes.net",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2569,
+			"friends_count": 4985,
+			"listed_count": 313,
+			"created_at": "Tue Jul 02 11:30:45 +0000 2013",
+			"favourites_count": 14802,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 44553,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1576114396400590848/7zquaAE__normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1576114396400590848/7zquaAE__normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1562961949/1659387530",
+			"profile_link_color": "3B94D9",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 15,
+		"favorite_count": 11,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "fr",
+		"text": "Space #okcinfo vendredi prochain !\n\nOn va brièvement aborder la fin de l'affaire belge OKC-Spatz, rejet de l'appel des accusés \n\nLes suites de l'instruction en France\n\nLa lente prise de conscience du Bouddhisme Tibétain sur sa propre responsabilité. \n\nhttps://t.co/6SB1qd6MqP"
+	}
+]
