713da9c7c5ed645d5850f20ae9f13b62c0779584
Ben Borges <email@benborges.xyz>
2023-01-31 09:18:13 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 1605,
-	"created_at": "Sat Jan 28 08:18:12 +0000 2023",
+	"tweets": 1606,
+	"created_at": "Tue Jan 31 08:18:12 +0000 2023",
 	"lang": "en"
 }
--- data/js/tweets/2023_01.js
+++ data/js/tweets/2023_01.js
@@ -1,6 +1,191 @@
 Grailbird.data.tweets_2023_01 = 
 [
 	{
+		"created_at": "Mon Jan 30 09:03:49 +0000 2023",
+		"id": 1619984679872102400,
+		"id_str": "1619984719227260934",
+		"full_text": "Hésitez pas à vous abonner !\nOn va faire de nouvelles vidéos, plus courtes, sur des questions reçues par emails ou commentaires.\n\nhttps://t.co/nXizpMRQtd https://t.co/PNOUJss4bx",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			153
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/nXizpMRQtd",
+					"expanded_url": "https://youtube.com/@OKCinfo",
+					"display_url": "youtube.com/@OKCinfo",
+					"indices": [
+						130,
+						153
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1619984675098738700,
+					"id_str": "1619984675098738688",
+					"indices": [
+						154,
+						177
+					],
+					"media_url": "http://pbs.twimg.com/media/FntXepzWQAAAiJj.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FntXepzWQAAAiJj.jpg",
+					"url": "https://t.co/PNOUJss4bx",
+					"display_url": "pic.twitter.com/PNOUJss4bx",
+					"expanded_url": "https://twitter.com/OKCinfor/status/1619984679872102402/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1080,
+							"h": 887,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 887,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 558,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1619984675098738700,
+					"id_str": "1619984675098738688",
+					"indices": [
+						154,
+						177
+					],
+					"media_url": "http://pbs.twimg.com/media/FntXepzWQAAAiJj.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FntXepzWQAAAiJj.jpg",
+					"url": "https://t.co/PNOUJss4bx",
+					"display_url": "pic.twitter.com/PNOUJss4bx",
+					"expanded_url": "https://twitter.com/OKCinfor/status/1619984679872102402/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1080,
+							"h": 887,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 887,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 558,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1097520031955587100,
+			"id_str": "1097520031955587073",
+			"name": "Initiative de Justice OKCinfo",
+			"screen_name": "OKCinfor",
+			"location": " 🇫🇷🇧🇪🇪🇸🇵🇹",
+			"description": "2023🇫🇷Instruction en cours | \n2022🇧🇪Rejet du pourvoi en cassation des accusés | \n2020🇧🇪Condamné 5 ans sursis | \n2017🇧🇪Condamné 4 ans sursis | \n📞@rMdes_",
+			"url": "https://t.co/5PicHe4Xbq",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/5PicHe4Xbq",
+							"expanded_url": "https://okcinfo.news",
+							"display_url": "okcinfo.news",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 227,
+			"friends_count": 391,
+			"listed_count": 1,
+			"created_at": "Mon Feb 18 15:35:41 +0000 2019",
+			"favourites_count": 396,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1314,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1487025431396114438/_3Em3Zfy_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1487025431396114438/_3Em3Zfy_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1097520031955587073/1667149629",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "fr",
+		"text": "Hésitez pas à vous abonner !\nOn va faire de nouvelles vidéos, plus courtes, sur des questions reçues par emails ou commentaires.\n\nhttps://t.co/nXizpMRQtd https://t.co/PNOUJss4bx"
+	},
+	{
 		"created_at": "Fri Jan 27 08:22:02 +0000 2023",
 		"id": 1618886621574860800,
 		"id_str": "1618887039453401090",
