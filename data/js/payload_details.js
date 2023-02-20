f6cf6807f2745bc826425ba255744e666ced13bc
Ben Borges <email@benborges.xyz>
2023-02-20 09:18:13 +0100

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 1617,
-	"created_at": "Thu Feb 16 08:18:12 +0000 2023",
+	"tweets": 1618,
+	"created_at": "Mon Feb 20 08:18:12 +0000 2023",
 	"lang": "en"
 }
--- data/js/tweets/2023_02.js
+++ data/js/tweets/2023_02.js
@@ -1,6 +1,217 @@
 Grailbird.data.tweets_2023_02 = 
 [
 	{
+		"created_at": "Sun Feb 19 17:43:23 +0000 2023",
+		"id": 1627357562281554000,
+		"id_str": "1627363227372777473",
+		"full_text": "Il y a 3 ans, deuxième appel en cassation rejeté et on était reparti pour 8 mois de procès à Liège, en pleine pandémie...\n\nLe temps passe.. Mais nous on oublie pas !\n\nEt avec la même énergie, on les attends au tournant en France et cette fois ci, c'est la finale ! https://t.co/BgPK2XExc0",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			264
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1627357555864354800,
+					"id_str": "1627357555864354817",
+					"indices": [
+						265,
+						288
+					],
+					"media_url": "http://pbs.twimg.com/media/FpWJEy_XwAE8ILH.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FpWJEy_XwAE8ILH.jpg",
+					"url": "https://t.co/BgPK2XExc0",
+					"display_url": "pic.twitter.com/BgPK2XExc0",
+					"expanded_url": "https://twitter.com/OKCinfor/status/1627357562281553922/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 860,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 487,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1507,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1627357555864354800,
+					"id_str": "1627357555864354817",
+					"indices": [
+						265,
+						288
+					],
+					"media_url": "http://pbs.twimg.com/media/FpWJEy_XwAE8ILH.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FpWJEy_XwAE8ILH.jpg",
+					"url": "https://t.co/BgPK2XExc0",
+					"display_url": "pic.twitter.com/BgPK2XExc0",
+					"expanded_url": "https://twitter.com/OKCinfor/status/1627357562281553922/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 860,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 487,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1507,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1627357559819497500,
+					"id_str": "1627357559819497473",
+					"indices": [
+						265,
+						288
+					],
+					"media_url": "http://pbs.twimg.com/media/FpWJFBuWcAEt3SU.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FpWJFBuWcAEt3SU.jpg",
+					"url": "https://t.co/BgPK2XExc0",
+					"display_url": "pic.twitter.com/BgPK2XExc0",
+					"expanded_url": "https://twitter.com/OKCinfor/status/1627357562281553922/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 481,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 764,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 764,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1097520031955587100,
+			"id_str": "1097520031955587073",
+			"name": "Initiative de Justice OKCinfo",
+			"screen_name": "OKCinfor",
+			"location": " 🇫🇷🇧🇪🇪🇸🇵🇹",
+			"description": "2023🇫🇷Instruction en cours | \n2022🇧🇪Rejet du pourvoi en cassation des accusés | \n2020🇧🇪Condamné 5 ans sursis | \n2017🇧🇪Condamné 4 ans sursis | \n📞@rMdes_",
+			"url": "https://t.co/uRVeG10dZv",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/uRVeG10dZv",
+							"expanded_url": "https://okcinfo.news",
+							"display_url": "okcinfo.news",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 231,
+			"friends_count": 411,
+			"listed_count": 1,
+			"created_at": "Mon Feb 18 15:35:41 +0000 2019",
+			"favourites_count": 401,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1359,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1487025431396114438/_3Em3Zfy_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1487025431396114438/_3Em3Zfy_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1097520031955587073/1667149629",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 6,
+		"favorite_count": 10,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "fr",
+		"text": "Il y a 3 ans, deuxième appel en cassation rejeté et on était reparti pour 8 mois de procès à Liège, en pleine pandémie...\n\nLe temps passe.. Mais nous on oublie pas !\n\nEt avec la même énergie, on les attends au tournant en France et cette fois ci, c'est la finale ! https://t.co/BgPK2XExc0"
+	},
+	{
 		"created_at": "Wed Feb 15 17:10:56 +0000 2023",
 		"id": 1625904051752775700,
 		"id_str": "1625905512507224078",
