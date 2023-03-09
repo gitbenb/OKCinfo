9893b2ad48b8ee943d21cce6d291301d713e7d67
Ben Borges <email@benborges.xyz>
2023-03-09 09:18:13 +0100

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 1622,
-	"created_at": "Wed Mar 01 08:18:11 +0000 2023",
+	"tweets": 1624,
+	"created_at": "Thu Mar 09 08:18:12 +0000 2023",
 	"lang": "en"
 }
---
+++ data/js/tweets/2023_03.js
@@ -0,0 +1,418 @@
+Grailbird.data.tweets_2023_03 = 
+[
+	{
+		"created_at": "Wed Mar 08 22:11:46 +0000 2023",
+		"id": 1633408602659496000,
+		"id_str": "1633591362351136769",
+		"full_text": "On l'appellait l'œuf, c'était une sorte de hutte sommaire, sans eau, électricité, chauffage ou fenêtre qui entre les années 70 et 90 a été utilisé par OKC-Spatz pour y enfermer des individus  gênants (+ d'une année) et aussi des enfants jusqu'à 1 semaine. https://t.co/IFllBzuGGD",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			255
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1633408584854765600,
+					"id_str": "1633408584854765571",
+					"indices": [
+						256,
+						279
+					],
+					"media_url": "http://pbs.twimg.com/media/FqsIdClXgAMPND-.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FqsIdClXgAMPND-.jpg",
+					"url": "https://t.co/IFllBzuGGD",
+					"display_url": "pic.twitter.com/IFllBzuGGD",
+					"expanded_url": "https://twitter.com/rMdes_/status/1633408602659495939/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1613,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 455,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 803,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1633408584854765600,
+					"id_str": "1633408584854765571",
+					"indices": [
+						256,
+						279
+					],
+					"media_url": "http://pbs.twimg.com/media/FqsIdClXgAMPND-.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FqsIdClXgAMPND-.jpg",
+					"url": "https://t.co/IFllBzuGGD",
+					"display_url": "pic.twitter.com/IFllBzuGGD",
+					"expanded_url": "https://twitter.com/rMdes_/status/1633408602659495939/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1613,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 455,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 803,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1562961949,
+			"id_str": "1562961949",
+			"name": "Ricardo Mendes",
+			"screen_name": "rMdes_",
+			"location": "🇪🇺🇵🇹🇺🇦🇧🇪😷💉💉💉💉",
+			"description": "Passionné par la Tech, l'information, la démocratie, Justice et discernement | Partie civile 🇧🇪 et Porte-parole @OKCinfor ¦   \n🐘@rmdes@mstdn.social",
+			"url": "https://t.co/zNucbsHeDX",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/zNucbsHeDX",
+							"expanded_url": "https://rmendes.net/",
+							"display_url": "rmendes.net",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2590,
+			"friends_count": 4439,
+			"listed_count": 314,
+			"created_at": "Tue Jul 02 11:30:45 +0000 2013",
+			"favourites_count": 15115,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 45597,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1626312990881456128/fnNyjmnb_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1626312990881456128/fnNyjmnb_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1562961949/1677575068",
+			"profile_link_color": "3B94D9",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 8,
+		"favorite_count": 9,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "fr",
+		"text": "On l'appellait l'œuf, c'était une sorte de hutte sommaire, sans eau, électricité, chauffage ou fenêtre qui entre les années 70 et 90 a été utilisé par OKC-Spatz pour y enfermer des individus  gênants (+ d'une année) et aussi des enfants jusqu'à 1 semaine. https://t.co/IFllBzuGGD"
+	},
+	{
+		"created_at": "Wed Mar 08 22:11:43 +0000 2023",
+		"id": 1633418750174154800,
+		"id_str": "1633591349797691392",
+		"full_text": "\"petit\" complément d'info sur la question... Qui n'aborde même pas \"la salle des chaussures\" où des enfants étaient régulièrement enfermé pendant des heures \"pour se calmer\", une salle de quelques mètres carrés, remplie de chaussures, fermée avec un loquet, sans lumière. https://t.co/WYFIR78Znb",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			271
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/WYFIR78Znb",
+					"expanded_url": "https://twitter.com/OKCinfor/status/1633416970413506562",
+					"display_url": "twitter.com/OKCinfor/statu…",
+					"indices": [
+						272,
+						295
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1562961949,
+			"id_str": "1562961949",
+			"name": "Ricardo Mendes",
+			"screen_name": "rMdes_",
+			"location": "🇪🇺🇵🇹🇺🇦🇧🇪😷💉💉💉💉",
+			"description": "Passionné par la Tech, l'information, la démocratie, Justice et discernement | Partie civile 🇧🇪 et Porte-parole @OKCinfor ¦   \n🐘@rmdes@mstdn.social",
+			"url": "https://t.co/zNucbsHeDX",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/zNucbsHeDX",
+							"expanded_url": "https://rmendes.net/",
+							"display_url": "rmendes.net",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2590,
+			"friends_count": 4439,
+			"listed_count": 314,
+			"created_at": "Tue Jul 02 11:30:45 +0000 2013",
+			"favourites_count": 15115,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 45597,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1626312990881456128/fnNyjmnb_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1626312990881456128/fnNyjmnb_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1562961949/1677575068",
+			"profile_link_color": "3B94D9",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1633416970413506600,
+		"quoted_status_id_str": "1633416970413506562",
+		"quoted_status_permalink": {
+			"url": "https://t.co/WYFIR78Znb",
+			"expanded": "https://twitter.com/OKCinfor/status/1633416970413506562",
+			"display": "twitter.com/OKCinfor/statu…"
+		},
+		"quoted_status": {
+			"created_at": "Wed Mar 08 10:38:47 +0000 2023",
+			"id": 1633416970413506600,
+			"id_str": "1633416970413506562",
+			"full_text": "@rMdes_ C'est pas le seul endroit où des enfants/adolescents ont été enfermé pendant de longues périodes.\n\nIl y a plusieurs \"chamkhan\" sorte de cabanons en bois en mode Hobbit, avec une micro fenêtre et une porte ronde qui se trouvait à l'écart du château https://t.co/97LqWI5He1\n\nJe me… https://t.co/w8JW7d0Qow",
+			"truncated": false,
+			"display_text_range": [
+				8,
+				311
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "rMdes_",
+						"name": "Ricardo Mendes",
+						"id": 1562961949,
+						"id_str": "1562961949",
+						"indices": [
+							0,
+							7
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/97LqWI5He1",
+						"expanded_url": "https://okcinfo.news/2020/lieux/nyima-dzong-aka-chateau-de-soleils/",
+						"display_url": "okcinfo.news/2020/lieux/nyi…",
+						"indices": [
+							256,
+							279
+						]
+					},
+					{
+						"url": "https://t.co/w8JW7d0Qow",
+						"expanded_url": "https://twitter.com/i/web/status/1633416970413506562",
+						"display_url": "twitter.com/i/web/status/1…",
+						"indices": [
+							288,
+							311
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+			"in_reply_to_status_id": 1633408602659496000,
+			"in_reply_to_status_id_str": "1633408602659495939",
+			"in_reply_to_user_id": 1562961949,
+			"in_reply_to_user_id_str": "1562961949",
+			"in_reply_to_screen_name": "rMdes_",
+			"user": {
+				"id": 1097520031955587100,
+				"id_str": "1097520031955587073",
+				"name": "Initiative Justice OKCinfo",
+				"screen_name": "OKCinfor",
+				"location": " 🇫🇷🇧🇪🇪🇸🇵🇹",
+				"description": "2023🇫🇷Instruction en cours | \n2022🇧🇪Rejet du pourvoi en cassation des accusés | \n2020🇧🇪Condamné 5 ans sursis | \n2017🇧🇪Condamné 4 ans sursis | \n📞@rMdes_",
+				"url": "https://t.co/uRVeG10dZv",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/uRVeG10dZv",
+								"expanded_url": "https://okcinfo.news",
+								"display_url": "okcinfo.news",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 234,
+				"friends_count": 411,
+				"listed_count": 2,
+				"created_at": "Mon Feb 18 15:35:41 +0000 2019",
+				"favourites_count": 403,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 1387,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "F5F8FA",
+				"profile_background_image_url": null,
+				"profile_background_image_url_https": null,
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1487025431396114438/_3Em3Zfy_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1487025431396114438/_3Em3Zfy_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/1097520031955587073/1667149629",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": true,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 4,
+			"favorite_count": 3,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "fr"
+		},
+		"retweet_count": 6,
+		"favorite_count": 6,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "fr",
+		"text": "\"petit\" complément d'info sur la question... Qui n'aborde même pas \"la salle des chaussures\" où des enfants étaient régulièrement enfermé pendant des heures \"pour se calmer\", une salle de quelques mètres carrés, remplie de chaussures, fermée avec un loquet, sans lumière. https://t.co/WYFIR78Znb"
+	}
+]
