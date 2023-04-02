955c1b7f5acc36550cca110f7fc6cd43f043362a
Ben Borges <email@benborges.xyz>
2023-04-02 10:18:13 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 1657,
-	"created_at": "Mon Mar 27 08:18:13 +0000 2023",
+	"tweets": 1658,
+	"created_at": "Sun Apr 02 08:18:12 +0000 2023",
 	"lang": "en"
 }
---
+++ data/js/tweets/2023_04.js
@@ -0,0 +1,151 @@
+Grailbird.data.tweets_2023_04 = 
+[
+	{
+		"created_at": "Sat Apr 01 18:03:44 +0000 2023",
+		"id": 1642226253791887400,
+		"id_str": "1642226253791887361",
+		"full_text": "L'affaire OKC 🇧🇪🇫🇷🇵🇹🇪🇸\nLes perquisitions de police de 1997 contre la dérive sectaire Ogyen Kunzang Choling 🇧🇪\n\nUne rétrospective avec @rMdes_ porte-parole @OKCinfor partie civile dans l'affaire Belge et @JL_Adenor\nrédacteur-chef adjoint @Charlie_Hebdo_\n\nhttps://t.co/QNhhZhtm7s",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			277
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "rMdes_",
+					"name": "Ricardo Mendes",
+					"id": 1562961949,
+					"id_str": "1562961949",
+					"indices": [
+						134,
+						141
+					]
+				},
+				{
+					"screen_name": "OKCinfor",
+					"name": "Initiative de Justice OKCinfo",
+					"id": 1097520031955587100,
+					"id_str": "1097520031955587073",
+					"indices": [
+						155,
+						164
+					]
+				},
+				{
+					"screen_name": "JL_Adenor",
+					"name": "Jean-Loup Adénor",
+					"id": 432518496,
+					"id_str": "432518496",
+					"indices": [
+						203,
+						213
+					]
+				},
+				{
+					"screen_name": "Charlie_Hebdo_",
+					"name": "Charlie Hebdo",
+					"id": 65416867,
+					"id_str": "65416867",
+					"indices": [
+						237,
+						252
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/QNhhZhtm7s",
+					"expanded_url": "https://twitter.com/i/spaces/1djGXlMzqdjGZ",
+					"display_url": "twitter.com/i/spaces/1djGX…",
+					"indices": [
+						254,
+						277
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3309353195,
+			"id_str": "3309353195",
+			"name": "OKCinfo Research",
+			"screen_name": "OKCinformation",
+			"location": "Belgique",
+			"description": "#OKCinfo is an ongoing Justice initiative from ex-OKC born kids now civil parties to drag Ogyen Kunzang Choling/Robert Spatz Lama Kunzang to justice & jail it!",
+			"url": "https://t.co/zYcyQtvumv",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/zYcyQtvumv",
+							"expanded_url": "https://okcinfo.news/en",
+							"display_url": "okcinfo.news/en",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 211,
+			"friends_count": 383,
+			"listed_count": 7,
+			"created_at": "Fri Jun 05 15:00:16 +0000 2015",
+			"favourites_count": 356,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1771,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/656530168312262656/Ujovmh2w_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/656530168312262656/Ujovmh2w_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3309353195/1625149299",
+			"profile_link_color": "DD2E44",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "fr",
+		"text": "L'affaire OKC 🇧🇪🇫🇷🇵🇹🇪🇸\nLes perquisitions de police de 1997 contre la dérive sectaire Ogyen Kunzang Choling 🇧🇪\n\nUne rétrospective avec @rMdes_ porte-parole @OKCinfor partie civile dans l'affaire Belge et @JL_Adenor\nrédacteur-chef adjoint @Charlie_Hebdo_\n\nhttps://t.co/QNhhZhtm7s"
+	}
+]
