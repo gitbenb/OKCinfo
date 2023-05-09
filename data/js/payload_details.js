7fd0e6d6888eb9b4de8a23ecfd3505b6dac228f8
Ben Borges <email@benborges.xyz>
2023-05-09 10:18:13 +0200

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 1717,
-	"created_at": "Mon May 08 08:18:12 +0000 2023",
+	"tweets": 1718,
+	"created_at": "Tue May 09 08:18:12 +0000 2023",
 	"lang": "en"
 }
--- data/js/tweets/2023_05.js
+++ data/js/tweets/2023_05.js
@@ -1,6 +1,206 @@
 Grailbird.data.tweets_2023_05 = 
 [
 	{
+		"created_at": "Tue May 09 05:44:39 +0000 2023",
+		"id": 1655810996705935400,
+		"id_str": "1655810996705935360",
+		"full_text": "Bing relaying disinformation from OKC as default answer to who is Lama Kunzang Dorje alias Robert Spatz.\n\nReality : Robert Spatz — Wikipédia https://t.co/SMmH5wFqP1 \n\n #TibetanBuddhism #Bing https://t.co/B718N5i1lE",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			190
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "TibetanBuddhism",
+					"indices": [
+						168,
+						184
+					]
+				},
+				{
+					"text": "Bing",
+					"indices": [
+						185,
+						190
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/SMmH5wFqP1",
+					"expanded_url": "https://fr.wikipedia.org/wiki/Robert_Spatz",
+					"display_url": "fr.wikipedia.org/wiki/Robert_Sp…",
+					"indices": [
+						141,
+						164
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1655810994235474000,
+					"id_str": "1655810994235473921",
+					"indices": [
+						191,
+						214
+					],
+					"media_url": "http://pbs.twimg.com/media/FvqfU46XgAEcFQ4.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FvqfU46XgAEcFQ4.jpg",
+					"url": "https://t.co/B718N5i1lE",
+					"display_url": "pic.twitter.com/B718N5i1lE",
+					"expanded_url": "https://twitter.com/OKCinformation/status/1655810996705935360/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 421,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 743,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1745,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1655810994235474000,
+					"id_str": "1655810994235473921",
+					"indices": [
+						191,
+						214
+					],
+					"media_url": "http://pbs.twimg.com/media/FvqfU46XgAEcFQ4.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FvqfU46XgAEcFQ4.jpg",
+					"url": "https://t.co/B718N5i1lE",
+					"display_url": "pic.twitter.com/B718N5i1lE",
+					"expanded_url": "https://twitter.com/OKCinformation/status/1655810996705935360/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 421,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 743,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1745,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3309353195,
+			"id_str": "3309353195",
+			"name": "OKCinfo Research",
+			"screen_name": "OKCinformation",
+			"location": "Belgique",
+			"description": "#OKCinfo is an ongoing Justice initiative from ex-OKC born kids now civil parties to drag Ogyen Kunzang Choling/Robert Spatz Lama Kunzang to justice & jail it!",
+			"url": "https://t.co/zYcyQtvumv",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/zYcyQtvumv",
+							"expanded_url": "https://okcinfo.news/en",
+							"display_url": "okcinfo.news/en",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 216,
+			"friends_count": 384,
+			"listed_count": 7,
+			"created_at": "Fri Jun 05 15:00:16 +0000 2015",
+			"favourites_count": 369,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1847,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/656530168312262656/Ujovmh2w_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/656530168312262656/Ujovmh2w_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3309353195/1625149299",
+			"profile_link_color": "DD2E44",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ht",
+		"text": "Bing relaying disinformation from OKC as default answer to who is Lama Kunzang Dorje alias Robert Spatz.\n\nReality : Robert Spatz — Wikipédia https://t.co/SMmH5wFqP1 \n\n #TibetanBuddhism #Bing https://t.co/B718N5i1lE"
+	},
+	{
 		"created_at": "Mon May 08 07:36:30 +0000 2023",
 		"id": 1655476296317632500,
 		"id_str": "1655476755031883776",
